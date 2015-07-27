7aab4a8ebe2d7af69d8126d22a036670becff73d
U-inspiron530S\ndwork <ndwork@inspiron530S.(none)>
2015-07-26 18:31:14 -0700

    Finishing rename of file.

--- labelImgPts.m
+++ labelImgPts.m
@@ -1,10 +1,10 @@
 
-function labelImgFeatures( features, varargin )
-  % labelImgFeatures( features [, scale] )
+function labelImgPts( pts, varargin )
+  % labelImgFeatures( pts [, scale] )
   % This function puts the index of the point onto the image
   % It assumes that the image is already displayed and the figure
   %   is set to the current figure.
-  % features - an Nx2 array where the first column is the x location
+  % pts - an Nx2 array where the first column is the x location
   %   and the second column is the y location
   % scale (optional) - scales the points to display correctly on a scaled
   %   image.  Its default value is 1.
@@ -17,11 +17,11 @@ function labelImgFeatures( features, varargin )
   p.parse( varargin{:} );
   scale = p.Results.scale;
 
-  scaledFeatures = round( features * scale );
+  scaledPts = round( pts * scale );
 
-  [nPts,~] = size(scaledFeatures);
+  [nPts,~] = size(scaledPts);
   for i=1:nPts
-    th = text( scaledFeatures(i,1), scaledFeatures(i,2), num2str(i), ...
+    th = text( scaledPts(i,1), scaledPts(i,2), num2str(i), ...
       'FontSize', 30);
   end
 
