b82bca51b9011da5b75c28c67b007a81c22821bf
ndwork <ndwork@rescomp-13-276244.stanford.edu>
2016-01-29 19:13:13 -0800

    Adding padData to repository.

---
+++ padData.m
@@ -0,0 +1,55 @@
+
+function padded = padData( data, N, varargin )
+  % padded = padData( data, N [, padValue ] )
+  % data - an array to be padded
+  % N - specifies the final size of the padded data
+
+  defaultPadValue = 0;
+  p = inputParser;
+  p.addOptional( 'padValue', defaultPadValue );
+  p.parse( varargin{:} );
+  padValue = p.Results.padValue;
+
+  sData = size(data);
+  tooSmall = max( sData > N );
+  if tooSmall>0, error('Padded size is too small.'); end;
+
+  nDimData = ndims( data );
+  if nDimData==2 && min( size(data) ) == 1
+    nDimData = 1;
+  end
+
+  if numel(N) ~= nDimData
+    error('Incorrect size of padded data specified');
+  end
+
+  if nDimData == 1
+    if isrow( data )
+      padded = ones( 1, N ) * padValue;
+    else
+      padded = ones( N, 1 ) * padValue;
+    end
+    nData = numel( data );
+    minY = ceil( N/2 - nData/2 + 1 );
+    padded( minY : minY+nData-1 ) = data;
+
+  elseif nDimData == 2
+    padded = zeros( N );
+    sData = size( data );
+    minY = ceil( N(1)/2 - sData(1)/2 + 1 );
+    minX = ceil( N(2)/2 - sData(2)/2 + 1 );
+    padded( minY : minY+sData(1)-1, minX:minX:sData(2)-1 ) = data;
+
+  elseif nDimData == 3
+    padded = zeros( N );
+    sData = size( data );
+    minY = ceil( N(1)/2 - sData(1)/2 + 1 );
+    minX = ceil( N(2)/2 - sData(2)/2 + 1 );
+    minZ = ceil( N(3)/2 - sData(3)/2 + 1 );
+    padded ( minY : minY + sData(1)-1, ...
+             minX : minX + sData(2)-1, ...
+             minZ : minZ + sData(3)-1  ) = data;
+  end
+
+
+end
--- testDworkLib.m
+++ testDworkLib.m
@@ -2,103 +2,119 @@
 function testDworkLib
   clear; close all; rng(1);
 
-  %% bilateralFilter
-  fprintf('\nTesting bilateralFilter: \n');
-  img = phantom();  sImg = size(img);
-  noiseSig = 0.1;
-  noise = normrnd( 0, noiseSig, sImg(1), sImg(2) );
-  noisyImg = img + noise;
-  denoisedImg = bilateralFilter( img, 'sigmaR', 0.1 );
-  figure; imshow( [noisyImg, denoisedImg], [] );
-  title('Bilateral Filter Result');
+%   %% bilateralFilter
+%   fprintf('\nTesting bilateralFilter: \n');
+%   img = phantom();  sImg = size(img);
+%   noiseSig = 0.1;
+%   noise = normrnd( 0, noiseSig, sImg(1), sImg(2) );
+%   noisyImg = img + noise;
+%   denoisedImg = bilateralFilter( img, 'sigmaR', 0.1 );
+%   figure; imshow( [noisyImg, denoisedImg], [] );
+%   title('Bilateral Filter Result');
+% 
+%   %% isEven - 1D data
+%   fprintf( '\nTesting isEven (1D): \n');
+%   A1 = rand(5,1);
+%   A1odd = 0.5 * ( A1 + flipud(A1) );
+%   if ~isEven( A1odd )
+%     err('isEven (1D) failed');
+%   else
+%     disp('isEven (1D) passed');
+%   end
+%   
+%   %% isEven - 2D data
+%   fprintf( '\nTesting isEven (2D): \n');
+%   A1 = rand(5,5);
+%   A1odd = 0.5 * ( A1 + rot90(A1,2) );
+%   if ~isEven( A1odd )
+%     err('isEven (2D) failed');
+%   else
+%     disp('isEven (2D) passed');
+%   end
+%   
+%   %% isHermitian - 1D data
+%   fprintf( '\nTesting isHermitian (1D): \n');
+%   A1 = rand(5,1);
+%   A1even = 0.5 * ( A1 + flipud(A1) );
+%   A1odd = 0.5 * ( A1 - flipud(A1) );
+%   A1hermitian = A1even + 1i * A1odd;
+%   if ~isHermitian( A1hermitian )
+%     err('isEven (1D) failed');
+%   else
+%     disp('isEven (1D) passed');
+%   end
+% 
+%   %% isHermitian - 2D data
+%   fprintf( '\nTesting isHermitian (2D): \n');
+%   A1 = rand(5,1);
+%   A1even = 0.5 * ( A1 + rot90(A1,2) );
+%   A1odd = 0.5 * ( A1 - rot90(A1,2) );
+%   A1hermitian = A1even + 1i * A1odd;
+%   if ~isHermitian( A1hermitian )
+%     err('isEven (2D) failed');
+%   else
+%     disp('isEven (2D) passed');
+%   end
+% 
+%   %% isOdd - 1D data
+%   fprintf( '\nTesting isOdd (1D): \n');
+%   A1 = rand(5,1);
+%   A1odd = 0.5 * ( A1 - flipud(A1) );
+%   if ~isOdd( A1odd )
+%     err('isOdd (1D) failed');
+%   else
+%     disp('isOdd (1D) passed');
+%   end
+% 
+%   %% isOdd - 2D data
+%   fprintf( '\nTesting isOdd (2D): \n');
+%   A1 = rand(5,5);
+%   A1odd = 0.5 * ( A1 - rot90(A1,2) );
+%   if ~isOdd( A1odd )
+%     err('isOdd (2D) failed');
+%   else
+%     disp('isOdd (2D) passed');
+%   end
+%
+%   %% makeDftMatrix
+%   fprintf( '\nTesting makeDftMatrix: \n');
+%   M = 100;
+%   standard = fft( eye(M) );
+%   out = makeDftMatrix( M, M );
+%   diff = out - standard;
+%   err = max( abs( diff(:) ) );
+%   if err > 1d-14
+%     err(['makeDftMatrix failed with error ', num2str(err)]);
+%   else
+%     disp('makeDftMatrix passed');
+%   end
+%
+%   %% nonlocal mean
+%   fprintf('\nTesting nonlocal means: \n');
+%   img = phantom();  sImg = size(img);
+%   noiseSig = 0.1;
+%   noise = normrnd( 0, noiseSig, sImg(1), sImg(2) );
+%   noisyImg = img + noise;
+%   denoisedImg = nonlocalMeans( img, 'sigmaS', 0.02 );
+%   figure; imshow( [noisyImg, denoisedImg], [] );
+%   title('Nonlocal Means Result');
 
-  %% isEven - 1D data
-  fprintf( '\nTesting isEven (1D): \n');
-  A1 = rand(5,1);
-  A1odd = 0.5 * ( A1 + flipud(A1) );
-  if ~isEven( A1odd )
-    error('isEven (1D) failed');
-  else
-    disp('isEven (1D) passed');
-  end
-  
-  %% isEven - 2D data
-  fprintf( '\nTesting isEven (2D): \n');
-  A1 = rand(5,5);
-  A1odd = 0.5 * ( A1 + rot90(A1,2) );
-  if ~isEven( A1odd )
-    error('isEven (2D) failed');
-  else
-    disp('isEven (2D) passed');
-  end
-  
-  %% isHermitian - 1D data
-  fprintf( '\nTesting isHermitian (1D): \n');
-  A1 = rand(5,1);
-  A1even = 0.5 * ( A1 + flipud(A1) );
-  A1odd = 0.5 * ( A1 - flipud(A1) );
-  A1hermitian = A1even + 1i * A1odd;
-  if ~isHermitian( A1hermitian )
-    error('isEven (1D) failed');
-  else
-    disp('isEven (1D) passed');
-  end
+  %% padData
+  fprintf('\nTesting padData: \n');
+  padded = padData( [1 2 3], 5 );
+  err = norm( padded - [0 1 2 3 0] );
+  if err>0, error(['padData failed with error ', num2str(err)]); end;
+  padded = padData( [1 2 3], 4 );
+  err = norm( padded - [0 1 2 3] );
+  if err>0, error(['padData failed with error ', num2str(err)]); end;
+  padded = padData( [1 2], 4 );
+  err = norm( padded - [0 1 2 0] );
+  if err>0, error(['padData failed with error ', num2str(err)]); end;
+  padded = padData( [1 2], 5 );
+  err = norm( padded - [0 0 1 2 0] );
+  if err>0, error(['padData failed with error ', num2str(err)]); end;
+  disp('padData passed');
 
-  %% isHermitian - 2D data
-  fprintf( '\nTesting isHermitian (2D): \n');
-  A1 = rand(5,1);
-  A1even = 0.5 * ( A1 + rot90(A1,2) );
-  A1odd = 0.5 * ( A1 - rot90(A1,2) );
-  A1hermitian = A1even + 1i * A1odd;
-  if ~isHermitian( A1hermitian )
-    error('isEven (2D) failed');
-  else
-    disp('isEven (2D) passed');
-  end
-
-  %% isOdd - 1D data
-  fprintf( '\nTesting isOdd (1D): \n');
-  A1 = rand(5,1);
-  A1odd = 0.5 * ( A1 - flipud(A1) );
-  if ~isOdd( A1odd )
-    error('isOdd (1D) failed');
-  else
-    disp('isOdd (1D) passed');
-  end
-
-  %% isOdd - 2D data
-  fprintf( '\nTesting isOdd (2D): \n');
-  A1 = rand(5,5);
-  A1odd = 0.5 * ( A1 - rot90(A1,2) );
-  if ~isOdd( A1odd )
-    error('isOdd (2D) failed');
-  else
-    disp('isOdd (2D) passed');
-  end
-
-  %% makeDftMatrix
-  fprintf( '\nTesting makeDftMatrix: \n');
-  M = 100;
-  standard = fft( eye(M) );
-  out = makeDftMatrix( M, M );
-  diff = out - standard;
-  err = max( abs( diff(:) ) );
-  if err > 1d-14
-    error(['makeDftMatrix failed with error ', num2str(err)]);
-  else
-    disp('makeDftMatrix passed');
-  end
-
-  %% nonlocal mean
-  fprintf('\nTesting nonlocal means: \n');
-  img = phantom();  sImg = size(img);
-  noiseSig = 0.1;
-  noise = normrnd( 0, noiseSig, sImg(1), sImg(2) );
-  noisyImg = img + noise;
-  denoisedImg = nonlocalMeans( img, 'sigmaS', 0.02 );
-  figure; imshow( [noisyImg, denoisedImg], [] );
-  title('Nonlocal Means Result');
-  
   %% showLibs
   fprintf( '\nTesting showLibs: \n');
   showLibs();
