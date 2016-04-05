63bd44cc4b1edf779a53c433a69b4caf78a4561a
Nicholas Dwork <ndwork@Nicholass-Mac-Pro.local>
2016-04-05 10:25:22 -0700

    Cleaned up findAndTrackCorners.

--- findAndTrackCorners.m
+++ findAndTrackCorners.m
@@ -2,6 +2,16 @@
 function [pts1,pts2] = findAndTrackCorners( img1, img2, varargin )
   % corners = findAndTrackCorners( img1, img2 ...
   %   [, 'N', N, 'buffer', buffer, 'w', w, 'k', k, 'offset', offset ] )
+  %
+  % Inputs:
+  % img1/img2 - 2D arrays; find features in img1 and track into img2
+  % N - the number of features to find in img1
+  % buffer - the spacing between features in img1
+  % w - the feature width
+  % k - Harris corner parameter
+  % offset - 2 element array; the [y x] shift to center the search area
+  %
+  % Written by Nicholas Dwork 2016
 
   defaultN = 50;
   defaultBuffer = 20;
@@ -22,11 +32,5 @@ function [pts1,pts2] = findAndTrackCorners( img1, img2, varargin )
   offset = p.Results.offset;
 
   pts1 = findHarrisCorners(img1, N, buffer, w, k);
-  %showFeaturesOnImg( pts1, img1 );  title('Image 1');
-  figure; imshow(img1,[]); labelImgPts( pts1 );  title('Image 1');
-
   pts2 = trackFeatures( pts1, img1, img2, offset );
-  %showFeaturesOnImg( pts2, img2 );  title('Image 2');
-  figure; imshow(img2,[]); labelImgPts( pts2 );  title('Image 2');
 end
-
