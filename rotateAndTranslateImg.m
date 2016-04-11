8e2b98cdeec138c6b164e3c6bd0213deded0f0dd
ndwork <ndwork@ee-mbpro-ndwork.stanford.edu>
2016-04-11 10:47:12 -0700

    Adding rotateAndTranslateImg to repository.

---
+++ rotateAndTranslateImg.m
@@ -0,0 +1,40 @@
+
+function out = rotateAndTranslateImg( R, t, img, varargin )
+  % out = rotateAndTranslateImg( R, t, img [, extrapVal] )
+  %
+  % Inputs:
+  % R - a 2x2 rotation matrix
+  % t - a two element array specifying translation in pixels
+  % img - a 2D array
+  %
+  % Optional Inputs:
+  % extrapVal - value to put where extrapolation takes place
+  %
+  % Outputs:
+  % out - a 2D array - the transformed image
+  %
+  % Rotates and translates image according to the formula
+  %   newPt = R * pt + t;
+  %
+  % Written by Nicholas Dwork - Copyright 2016
+
+  defaultExtrapVal = 0;
+  p = inputParser;
+  p.addOptional( 'extrapVal', defaultExtrapVal );
+  p.parse( varargin{:} );
+  extrapVal = p.Results.extrapVal;
+
+  sImg = size( img );
+  ys = 1:sImg(1);
+  xs = 1:sImg(2);
+  [xs, ys] = meshgrid( xs, ys );
+  coords = [ xs(:)'; ys(:)' ];
+
+  tCoords = zeros( size(coords) );
+  tCoords(1,:) = coords(1,:) + t(1);
+  tCoords(2,:) = coords(2,:) + t(2);
+  RCoords = R * tCoords;
+  interped = interp2( img, RCoords(1,:), RCoords(2,:), ...
+    'linear', extrapVal );
+  out = reshape( interped, sImg );
+end
