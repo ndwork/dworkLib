976ab9d6080436ef5dcd9cbf6bcaa4f7aba1e63d
ndwork <ndwork@DN0a2323c3.SUNet>
2016-04-19 13:23:29 -0700

    Adding shearImg to repository.

---
+++ shearImg.m
@@ -0,0 +1,35 @@
+
+function out = shearImg( in, theta, dim )
+  % out = shearImg( in, theta, dim )
+  %
+  % shears the image along dimension dim
+  %
+  % Inputs:
+  % in - 2D array
+  % theta - rotation angle in radians
+  %
+  % Optional Inputs:
+  % dim - index of dimension to shear
+  %
+  % Written by Nicholas Dwork - Copyright 2016
+
+  if nargin < 3, dim = 1; end;
+
+  sIn = size( in );
+
+  midY = ceil((sIn(1)+1)/2);
+  midX = ceil((sIn(2)+1)/2);
+  ys = (1:sIn(1)) - midY;
+  xs = (1:sIn(2)) - midX;
+  ys = ys' * ones(1,sIn(2));
+  xs = ones(sIn(1),1) * xs;
+
+  if dim == 1
+    newYs = xs * tan(theta) + ys;
+    out = interp2( xs, ys, in, xs, newYs );
+  else
+    newXs = -ys * tan(theta) + xs;
+    out = interp2( xs, ys, in, newXs, ys );
+  end
+
+end
