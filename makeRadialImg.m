ae08fedf45ee01fd1bcd8791d1ec6b798318e503
ndwork <ndwork@DN0a229083.SUNet>
2016-06-05 13:16:06 -0700

    Bug fixed in padData. makeRadialImg added to repository.

---
+++ makeRadialImg.m
@@ -0,0 +1,24 @@
+
+function out = makeRadialImg( sImg )
+  % makes an image where each point represents the distance from the center
+  %
+  % out = makeRadialImg( sImg )
+  %
+  % Inputs:
+  % sImg - 2 element array specifying the size of the image
+  % 
+  % Outputs:
+  % out - output image
+  %
+  % Written by Nicholas Dwork - (c) 2016
+
+  y = 1:sImg(1);
+  x = 1:sImg(2);
+
+  y = y - ceil( (sImg(1)+1)/2 );
+  x = x - ceil( (sImg(1)+1)/2 );
+
+  [x,y] = meshgrid(x,y);
+
+  out = sqrt( x.*x + y.*y );
+end
