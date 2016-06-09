d5a4be23342c29ea486944e63434878788d4c6af
ndwork <ndwork@Nicholass-MacBook-Pro-5.local>
2016-06-08 17:37:32 -0700

    Added ability to set image scale in showFeaturesOnImg. Added shiftImg into repository.

---
+++ shiftImg.m
@@ -0,0 +1,36 @@
+% This software is offered under the GNU General Public License 3.0.  It 
+% is offered without any warranty expressed or implied, including the 
+% implied warranties of merchantability or fitness for a particular 
+% purpose.
+
+function out = shiftImg( img, shifts )
+  % out = shiftImg( img, shifts )
+  %
+  % Shift the image; shifts are defined according to circshift
+  % Regions with unknown data are zero filled.
+  %
+  % Inputs:
+  % img - a 2D array
+  % shifts - a 2 element array specifying vertical and horiztonal shift
+  %
+  % Outputs:
+  % out - the shifted image
+  %
+  % Written by Nicholas Dwork - Copyright 2016
+
+  out = circshift( img, shifts );
+
+  if shifts(1) > 0
+    out(1:shifts(1),:) = 0;
+  elseif shifts(1) < 0
+    out(end+shifts(1)+1:end,:) = 0;
+  end
+
+  if shifts(2) > 0
+    out(:,1:shifts(2)) = 0;
+  elseif shifts(2) < 0
+    out(:,end+shifts(2)+1:end) = 0;
+  end
+
+end
+
--- showFeaturesOnImg.m
+++ showFeaturesOnImg.m
@@ -1,16 +1,24 @@
 
-function showFeaturesOnImg( features, img )
-  % showFeaturesOnImg( features, img )
+function showFeaturesOnImg( features, img, varargin )
+  % showFeaturesOnImg( features, img [, scale] )
   %
   % Inputs:
   % features - 2D array of size Nx2
   %   N is the number of points
   %   The first/second column is the x/y location
   %
+  % Optional Inputs:
+  %   scale - 2 element array specifying image scale
+  %
   % Written by Nicholas Dwork - Copyright 2016
 
+  p = inputParser;
+  p.addOptional( 'scale', [] );
+  p.parse( varargin{:} );
+  scale = p.Results.scale;
+  
   figure;
-  imshow(img, []);
+  imshow(img, scale);
   hold on
   plot( features(:,1), features(:,2), 'r*');
   drawnow;
