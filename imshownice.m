c5629fdd8e69b9f2935de0245c7108b6541a4aef
ndwork <ndwork@DN0a225645.SUNet>
2016-08-29 18:02:07 -0700

    Upgrade to imshownice and makeEllipticalImage.

--- imshownice.m
+++ imshownice.m
@@ -1,11 +1,28 @@
 
-function imshownice( img, sdevScale )
+function imshownice( img, varargin )
+  % imshownice( img [, scale, 'sdevScale', sdevScale ] )
+  % show the image on the following scale
+  % meanImg - sdevScale*sdevImg, meanImg + sdevScale*sdevImg
+  %
+  % Written by Nicholas - Copyright 2016
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
 
-  if nargin < 2, sdevScale = 2.5; end;
+  defaultScale = 1.0;
+  defaultSDevScale = 2.5;
+  p = inputParser;
+  p.addOptional( 'scale', defaultScale );
+  p.addParameter( 'sdevScale', defaultSDevScale );
+  p.parse( varargin{:} );
+  sdevScale = p.Results.sdevScale;
+  scale = p.Results.scale;
 
   meanImg = mean( img(:) );
   sdevImg = std( img(:) );
-  
-  imshow( img, [ meanImg - sdevScale*sdevImg, meanImg + sdevScale*sdevImg ] );
 
+  tmp = imresize( img, scale );
+  imshow( tmp, [ meanImg - sdevScale*sdevImg, meanImg + sdevScale*sdevImg ] );
 end
--- makeEllipticalImage.m
+++ makeEllipticalImage.m
@@ -1,14 +1,14 @@
 
-function out = makeEllipticalImage( sImg, ry, rx, varargin )
-  % out = makeEllipticalImage( sImg, ry, rx [, rot ] )
+function out = makeEllipticalImage( sImg, varargin )
+  % out = makeEllipticalImage( sImg, [ rx, ry, rot ] )
   %
   % Creates an elliptical image.  The zero-levelset is where 
   % x^2/rx + y^2/ry = 1
   %
   % Inputs:
   % sImg - two element array specifying the size of the image [Ny,Nx]
-  % ry - the length of the semimajor axis in the y direction
-  % rz - the length of the semimajor axis in the x direction
+  % rx - the length of the semimajor axis in the horizontal direction
+  % ry - the length of the semimajor axis in the vertical direction
   % rot (optional) - clockwise rotation in radians
   %
   % Written by Nicholas Dwork
@@ -20,15 +20,21 @@ function out = makeEllipticalImage( sImg, ry, rx, varargin )
 
 
   defaultRot = 0;
+  defaultRx = floor(sImg(2)/2);
+  defaultRy = floor(sImg(1)/2);
   p = inputParser;
-  p.addOptional( 'rot', defaultRot );
+  p.addOptional( 'rx', defaultRx, @isnumeric );
+  p.addOptional( 'ry', defaultRy, @isnumeric );
+  p.addOptional( 'rot', defaultRot, @isnumeric );
   p.parse( varargin{:} );
+  rx = p.Results.rx;
+  ry = p.Results.ry;
   rot = p.Results.rot;
 
   y = (1:sImg(1)) - ceil((sImg(1)+1)/2);
   x = (1:sImg(2)) - ceil((sImg(2)+1)/2);
   [xs,ys] = meshgrid( x, y );
-  
+
   if rot~=0,
     pts = [ transpose(xs(:)); transpose(ys(:)); ];
     R = [ cos(rot) sin(rot); -sin(rot), cos(rot); ];
