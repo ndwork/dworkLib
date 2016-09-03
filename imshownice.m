f1760269a77433b8ee9323794123b395ea02d05f
ndwork <ndwork@ee-mbpro-ndwork.stanford.edu>
2016-09-02 18:42:00 -0700

    Added interpolation method to imshownice.

--- imshownice.m
+++ imshownice.m
@@ -1,8 +1,17 @@
 
 function imshownice( img, varargin )
-  % imshownice( img [, scale, 'sdevScale', sdevScale ] )
+  % imshownice( img [, scale, method, 'sdevScale', sdevScale ] )
   % show the image on the following scale
   % meanImg - sdevScale*sdevImg, meanImg + sdevScale*sdevImg
+  %
+  % Inputs:
+  % img - 2D array representing the image
+  %
+  % Optional Inputs:
+  % scale - factor to scale the size of the image for display
+  % method - when scaling method, interpolation method to use
+  %   default is 'nearest'
+  %   any method accepted by imresize is accepted for this parameter
   %
   % Written by Nicholas - Copyright 2016
   %
@@ -12,17 +21,20 @@ function imshownice( img, varargin )
   % purpose.
 
   defaultScale = 1.0;
+  defaultMethod = 'nearest';
   defaultSDevScale = 2.5;
   p = inputParser;
   p.addOptional( 'scale', defaultScale );
+  p.addOptional( 'method', defaultMethod );
   p.addParameter( 'sdevScale', defaultSDevScale );
   p.parse( varargin{:} );
-  sdevScale = p.Results.sdevScale;
   scale = p.Results.scale;
+  method = p.Results.method;
+  sdevScale = p.Results.sdevScale;
 
   meanImg = mean( img(:) );
   sdevImg = std( img(:) );
 
-  tmp = imresize( img, scale );
+  tmp = imresize( img, scale, method );
   imshow( tmp, [ meanImg - sdevScale*sdevImg, meanImg + sdevScale*sdevImg ] );
 end
