40e771f955fdd0c5337467a33b0acafa6c48c3df
ndwork <ndwork@DN525ban.SUNet>
2016-11-11 15:12:13 -0800

    Upgraded functions to list files in library that match search pattern.

--- dworkLib.m
+++ dworkLib.m
@@ -1,4 +1,16 @@
 
-function dworkLib
-  showLibFiles('dworkLib')
+function dworkLib( varargin )
+  % dworkLib( [ pattern ] )
+  %
+  % Optional Inputs
+  % pattern - file must contain this search pattern in order to be listed.
+  %
+  % Written by Nicholas - Copyright 2016
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  showLibFiles('dworkLib', varargin{:});
 end
--- showLibFiles.m
+++ showLibFiles.m
@@ -1,18 +1,34 @@
 
 function showLibFiles( varargin )
-  % showLibFiles( [ libName ] )
+  % showLibFiles( [ libName, pattern ] )
   % libName - an optional argument specifying the library name of interest
   % By default, this function shows all libraries.
   % If a library name is specified, then this function shows all the files
   % in that library.
+  %
+  % Optional Inputs:
+  % libName - an optional imput of the library to list
+  % pattern - an optional input specying a search pattern that must be
+  %   matched for a file to be listed
+  %
+  % Written by Nicholas - Copyright 2016
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
 
+  
   defaultLib = [];
+  defaultPattern = '.*';
   p = inputParser;
   p.addOptional( 'libName', defaultLib, @(x) true );
     % must specify validation function above to keep addOptional from
     % thinking that argument is a parameter's name.
+  p.addOptional( 'pattern', defaultPattern, @(x) true );
   p.parse( varargin{:} );
   libName = p.Results.libName;
+  pattern = p.Results.pattern;
 
   if isempty( libName )
     disp('Usage: showLibFiles( [ libName ] )' );
@@ -24,10 +40,12 @@ function showLibFiles( varargin )
     pathDirs = strsplit( myPath, ':' );
 
     for i=1:numel(pathDirs)
-      if ~isempty( regexp( pathDirs{i}, libName ) )
+      if ~isempty( regexp( pathDirs{i}, libName, 'ONCE' ) )
         files = dir( pathDirs{i} );
         for j=1:numel(files)
-          if isempty( regexp(files(j).name, '^\.' ) )
+          if isempty( regexp(files(j).name, '^\.', 'ONCE' ) ) && ...
+            ~isempty( regexp(files(j).name, pattern, 'ONCE' ) )
+
             disp( files(j).name );
           end
         end
