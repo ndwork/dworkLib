a071047abed8073cf216de6f9991642970ece6a5
ndwork <ndwork@ee-mbpro-ndwork.stanford.edu>
2017-03-16 12:04:28 -0700

    Added loopCount parameter to makeAGif. Added probeImg to the repository.

--- makeAGif.m
+++ makeAGif.m
@@ -1,11 +1,14 @@
 
 function makeAGif( varargin )
-  % makeAGif( inDir, outFile, delayTime )
+  % makeAGif( [ inDir, outFile, delayTime, 'loopCount', loopCount ] )
+  %
   %   This function makes a gif out of a set of images
   %   It assumes that all images are stored in their own directory (and
   %   no other files are stored in the directory with them).
   %   The images must be named so that they are listed in the order of display
   %   delayTime is the time between frames in the gif animation.
+  %
+  %   Inputs: 
   %   inDir (optional) - a string containing the name of the directory with input images
   %     By default, inDir is assumed to be './inDir'
   %   outFile (optional) - a string contianing the name of the gif file
@@ -13,21 +16,28 @@ function makeAGif( varargin )
   %   delayTime (optional) - the time between frames in the animation
   %     By default, delayTime is assumed to be 0.3 seconds
   %
-  % Written by Nicholas Dwork - (c) 2015
-
+  % Written by Nicholas Dwork - Copyright 2015
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
 
   defaultInDir = './inDir';
   defaultOutFile = 'makeAGifOut.gif';
   defaultDelayTime = 0.3;
+  defaultLoopCount = Inf;
 
   p = inputParser;
-  p.addOptional('inDir',defaultInDir,@(x) true);
-  p.addOptional('outFile',defaultOutFile,@(x) true);
-  p.addOptional('delayTime',defaultDelayTime,@isnumeric);
+  p.addOptional( 'inDir', defaultInDir, @(x) true );
+  p.addOptional( 'outFile', defaultOutFile, @(x) true );
+  p.addOptional( 'delayTime', defaultDelayTime, @isnumeric );
+  p.addParameter('loopCount', defaultLoopCount, @isnumeric );
   p.parse( varargin{:} );
   inDir = p.Results.inDir;
   outFile = p.Results.outFile;
   delayTime = p.Results.delayTime;
+  loopCount = p.Results.loopCount;
 
 
   imgs = dir( inDir );
@@ -36,13 +46,13 @@ function makeAGif( varargin )
 
   for i=1:numel(imgs)
     img = imread( [inDir,'/',imgs(i).name] );
-    if ndims(img)==2
+    if ismatrix(img)
       img = repmat(img,[1,1,3]);
     end
     [A,map] = rgb2ind(img,256);
 
     if i == 1;
-      imwrite(A,map,outFile,'gif','LoopCount',Inf,'DelayTime',delayTime);
+      imwrite(A,map,outFile,'gif','LoopCount',loopCount,'DelayTime',delayTime);
     else
       imwrite(A,map,outFile,'gif','WriteMode','append','DelayTime',delayTime);
     end
