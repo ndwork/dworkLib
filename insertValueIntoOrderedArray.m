f36780a734c767382568010492e4b7441ae30e73
Nicholas Dwork <ndwork@Nicholass-Mac-Pro.local>
2018-04-21 15:02:49 -0700

    Minor change to comments.

--- insertValueIntoOrderedArray.m
+++ insertValueIntoOrderedArray.m
@@ -10,7 +10,12 @@ function newArray = insertValueIntoOrderedArray( array, value )
   % array - a 1D (column or row) array
   % value - the value to insert
   %
-  % Written by Nicholas Dwork
+  % Written by Nicholas Dwork - Copyright 2017
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
 
   if numel( array ) == 0
     newArray = value;
