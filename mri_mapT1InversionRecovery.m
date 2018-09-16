5a1729e05259f345fedcee032d7b2d0847a471a2
Nicholas Dwork <ndwork@hotmail.com>
2018-09-15 17:42:13 -0700

    Initial commit of mri_mapT1InversionRecovery.m to the repository.

---
+++ mri_mapT1InversionRecovery.m
@@ -0,0 +1,60 @@
+
+
+function t1Map = mri_mapT1InversionRecovery( dataCube, TIs, varargin )
+  % t1Map = mri_mapT1InversionRecovery( dataCube, TIs, ...
+  %   [, 'mask', mask, 'verbose', verbose )
+  %
+  % Inputs:
+  % dataCube - a 3D array of size MxNxK.  Each k index corresponds to an
+  %   image taken with a different inversion time.
+  % TIs - a 1D array of size K specifying the inversion time of each image
+  %
+  % Optional Inputs:
+  % mask - a 2D array of size MxN.  Only map pixels with nonzero mask values.
+  % verbose - scalar; info statements made if verbose is nonzero
+  %
+  % Outputs:
+  % t1Map - a 2D array of size MxN; units are same as TIs
+  %
+  % Written by Nicholas Dwork - Copyright 2018
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  p = inputParser;
+  p.addParameter( 'mask', [], @(x) isnumeric(x) || islogical(x) );
+  p.addParameter( 'verbose', 0, @(x) isnumeric(x) || islogical(x) );
+  p.parse( varargin{:} );
+  mask = p.Results.mask;
+  verbose = p.Results.verbose;
+
+  sData = size( dataCube );
+  if numel( mask ) == 0, mask=ones( sData(1:2) ); end;
+  %showImageCube( mask.*abs(dataCube), showScale );  titlenice('t1 ir Data');
+
+  fminconOptions = optimoptions('fmincon','Display','off');
+  f = @(x,TIs) (x(1)+1i*x(2)) + (x(3)+1i*x(4))*exp(-TIs/x(5));
+  t1MapIRCols_ms = cell( sData(2), 1 );
+  p = parforProgress( sData(2) );
+  parfor i=1:sData(2)
+    if verbose ~= 0, p.progress( i, 10 );  end;                                            %#ok<PFBNS>
+    t1MapIRCol_ms = zeros( sData(1), 1 );                                                  %#ok<PFBNS>
+
+    for j=1:sData(1)
+      if mask(j,i) == 0, continue; end;
+      thisData = squeeze( dataCube(j,i,:) ); 
+      tmp = fmincon( @(tmp) norm( abs(thisData) - abs(f(tmp,TIs(:))) ), ...
+        [1;0;1;0;100], [],[],[],[], [-Inf;-Inf;-Inf;-Inf;0], [], [], fminconOptions );
+      t1MapIRCol_ms(j) = tmp(5);
+    end
+    t1MapIRCols_ms{i} = t1MapIRCol_ms;
+  end
+  p.clean;
+  t1Map = zeros( sData(1:2) );
+  for i=1:sData(2)
+    t1Map(:,i) = t1MapIRCols_ms{i};
+  end
+
+end
