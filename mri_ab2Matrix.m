927a4498fd897b59b40a3d8935fdfdfc950cc820
Nicholas Dwork <ndwork@hotmail.com>
2018-09-20 11:16:46 -0700

    Including mri_ab2Matrix.m and mri_mapOffRes in the repository.

---
+++ mri_ab2Matrix.m
@@ -0,0 +1,37 @@
+
+function M = mri_ab2Matrix( a, b )
+  % Converts the Cayley-Klein parameters into a matrix Matrix so that
+  % next([ Mxy; conj(Mxy); Mz]) = Matrix * [ Mxy; conj(Mxy); Mz ]
+  %
+  % Inputs
+  % a - a 1D array of the alpha CK parameters
+  % b - a 1D array of the beta CK parameters
+  %
+  % Outputs:
+  % M - a 3D array of size 3x3xN where N is the number of elements in a
+  %
+  % Written by Nicholas Dwork - Copyright 2018
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  if numel(a) ~= numel(b), error('a and b must be the same size'); end;
+
+  ca = conj(a);
+  cb = conj(b);
+  
+  M = zeros(3,3,numel(a));
+
+  M(1,1,:) = ca.^2;
+  M(1,2,:) = -b.^2;
+  M(1,3,:) = 2*ca.*b;
+  M(2,1,:) = -cb.^2;
+  M(2,2,:) = a.^2;
+  M(2,3,:) = 2*a.*cb;
+  M(3,1,:) = -ca.*cb;
+  M(3,2,:) = -a.*b;
+  M(3,3,:) = a.*ca-b.*cb;
+
+end
---
+++ mri_mapOffRes.m
@@ -0,0 +1,51 @@
+
+function [ offResMap, phaseOffsetMap ] = mri_mapOffRes( dataCube, TEs, varargin )
+  % [ offResMap, phaseOffsetMap ] = mri_mapOffRes( dataCube, TEs [, 'mask', mask ] )
+  %
+  % Inputs:
+  % dataCube - an MxNxK array of data where each k index represents an
+  %   image captured at a different echo time.
+  % TEs - a 1D array of size K representing the echo times.
+  %
+  % Optional Inputs:
+  % mask - an MxN array.  Any 0 element is data that doesn't get processed.
+  %   (default is all 1s)
+  %
+  % Ouptuts:
+  % offResMap - An MxN array representing off resonance frequency (in
+  % radians per unit of echo times)
+  % phaseOffsetMap - An MxN array representing the phase at time 0 (in
+  %   radians)
+  %
+  % Written by Nicholas Dwork, Copyright 2018
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  p = inputParser;
+  p.addParameter( 'mask', [], @(x) isnumeric(x) || islogical(x) );
+  p.parse( varargin{:} );
+  mask = p.Results.mask;
+
+  sData = size( dataCube );
+  if numel( mask ) == 0, mask=ones( sData(1:2) ); end;
+  %showImageCube( mask.*abs(dataCube), showScale );  titlenice('t1 ir Data');
+
+  sData = size( dataCube );
+  angles = angle( dataCube );
+
+  offResMap = zeros( sData(1:2) );
+  phaseOffsetMap = zeros( sData(1:2) );
+  for i=1:sData(2)
+    for j=1:sData(1)
+      if mask(j,i) == 0, continue; end;
+      coeffs = fitPolyToData( 1, TEs, angles(j,i,:) );
+      phaseOffsetMap(j,i) = coeffs(1);
+      offResMap(j,i) = coeffs(2);
+    end
+  end
+
+end
+
