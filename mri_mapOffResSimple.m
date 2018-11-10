a16d67586dc2684db1a7ab72e3f03941c4a78a79
Nicholas Dwork <ndwork@hotmail.com>
2018-11-09 16:08:38 -0800

    Adding mri_mapOffResSimple to the repository.

---
+++ mri_mapOffResSimple.m
@@ -0,0 +1,42 @@
+
+function [ offResMap, phaseOffsetMap ] = mri_mapOffResSimple( dataCube, TEs, varargin )
+  % [ offResMap, phaseOffsetMap ] = mri_mapOffRes( dataCube, TEs [, 'mask', mask ] )
+  %
+  % Inputs:
+  % dataCube - an MxNxK array of data where each k index represents an
+  %   image captured at a different echo time.
+  % TEs - a 1D array of size K representing the echo times.
+  %
+  % Optional Inputs:
+  % mask - an MxN array.  Any 0 element is data that doesn't get processed.
+  %   (default is all 1s)
+  %
+  % Ouptuts:
+  % offResMap - An MxN array representing off resonance frequency (in
+  %   radians per unit of echo times) according to a right handed rotation
+  % phaseOffsetMap - An MxN array representing the phase at time 0 (in
+  %   radians)
+  %
+  % Written by Nicholas Dwork, Copyright 2018
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  p = inputParser;
+  p.addParameter( 'mask', [], @(x) isnumeric(x) || islogical(x) );
+  p.parse( varargin{:} );
+  mask = p.Results.mask;
+
+  sData = size( dataCube );
+  if numel( mask ) == 0, mask=ones( sData(1:2) ); end;
+
+  angleOffset = angle( dataCube(:,:,2) .* conj( dataCube(:,:,1) ) );
+  offResMap = angleOffset / ( TEs(2) - TEs(1) );
+  offResMap = offResMap .* mask;
+  phaseOffsetMap = zeros( size(offResMap) );
+
+end
+
+
