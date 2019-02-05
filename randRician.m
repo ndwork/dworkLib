b2c663a1382dab257eeb9038161c4f4cb987a5a6
Nicholas Dwork <nicholasdwork@rad-0rng8wl-lt.ucsf.edu>
2019-02-05 11:19:29 -0800

    Added theta as an optional input to randRician.

--- randRician.m
+++ randRician.m
@@ -1,14 +1,23 @@
 
-function out = randRician( rNu, rSig )
+function out = randRician( rNu, rSig, varargin )
   % Generate a Rician distribution with parameters rNu and rSig according to
   % https://en.wikipedia.org/wiki/Rice_distribution#Related_distributions
   %
+  % out = randRician( rNu, rSig [, theta ] )
+  %
   % Inputs:
   % rNu - a single element or an array parameterizing the distribution
-  % rSig - a single element or an array specifying the standard deviation of the data.
-  %   Note that if rNu and rSig are both arrays, they must have the same size.
+  % rSig - a single element or an array (of results size)  specifying the standard deviation of the data.
+  %
+  % Optional Inputs:
+  % theta - a single element or an array (of results size) parameterizing the distribution
   %
   % Written by Nicholas Dwork, 2019
+
+  p = inputParser;
+  p.addOptional( 'theta', [], @isnumeric );
+  p.parse( varargin{:} );
+  theta = p.Results.theta;
 
   if numel( rNu ) > 1 && numel( rSig ) > 1 && ...
     max( size( rNu ) ~= size( rSig ) ) == 1
@@ -21,7 +30,9 @@ function out = randRician( rNu, rSig )
     sData = size( rNu );
   end
 
-  theta = pi * rand( sData );
+  if numel( theta ) == 0
+    theta = pi * rand( sData );
+  end
 
   x = randn( sData ) .* rSig + rNu .* cos(theta);
   y = randn( sData ) .* rSig + rNu .* sin(theta);
