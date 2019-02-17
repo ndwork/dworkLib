481c63293806912b0369247198d501dc8cfa4f08
Nicholas Dwork <ndwork@hotmail.com>
2019-02-16 18:00:25 -0800

    Adding weights to the proxL2L1 function (to permit weighted norm).

--- proxL2L1.m
+++ proxL2L1.m
@@ -1,8 +1,9 @@
 
-function out = proxL2L1( in, thresh )
-  % out = proxL2L1( in, thresh )
+function out = proxL2L1( in, thresh, weights )
+  % out = proxL2L1( in, thresh, weights )
   %
-  % Returns the proximal operator of f(x) = thresh * L2L1( x )
+  % Returns the proximal operator of f(x) = thresh * L2L1( x ), where
+  %   L2L1 is the (possibly weighted) L2,L1 norm.
   %
   % Inputs:
   % in - three dimensional array
@@ -15,11 +16,19 @@ function out = proxL2L1( in, thresh )
   % implied warranties of merchantability or fitness for a particular
   % purpose.
 
-  normsIn = norms( in, 2, 3 );
+  if nargin > 2
+    thresh = thresh .* weights;
+  end
+
+  sIn = size( in );
+  nDimsIn = numel( sIn );
+  normsIn = norms( in, 2, nDimsIn );
 
   scalingFactors = thresh ./ normsIn;
   scalingFactors( abs( normsIn ) <= thresh ) = 1;
-  scalingFactors = repmat( scalingFactors, [1 size(in,3)] );
+  repDims = ones( 1, nDimsIn );
+  repDims( end ) = sIn( end );
+  scalingFactors = repmat( scalingFactors, repDims );
   projsOntoL2Ball = in .* scalingFactors;
 
   out = in - projsOntoL2Ball;
