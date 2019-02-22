7cf2ea583ed788324199f0dc71ee6a95911a87f0
Nicholas Dwork <ndwork@hotmail.com>
2019-02-22 10:46:44 -0800

    Improved formatting for randPoissonDisc2D.

--- randPoissonDisc2D.m
+++ randPoissonDisc2D.m
@@ -1,13 +1,14 @@
+
 function [x,y] = randPoissonDisc2D( r, varargin )
-  % POISSONDIC2D Generate Poisson-disc-sampled points in two dimensions
-  % 
-  %   [X,Y] = RANDPD2D(R) randomly generates a collection of points (X,Y) in
-  %   rectangle (-0.5,-0.5),(0.5,0.5) that satisfy the minimum-distance 
-  %   requirement specified by R using the Brindson method (without using the
-  %   background grid).
-  %   
-  %   RANDPD2D uses the RAND function in its current/existing state.
-  %   
+  % RANDPOISSONDISC2D Generate Poisson-disc-sampled points in two dimensions
+  %
+  %   [X,Y] = RANDPOISSONDISC2D(R) randomly generates a collection of points (X,Y)
+  %     in rectangle (-0.5,-0.5),(0.5,0.5) that satisfy the minimum-distance 
+  %     requirement specified by R using the Brindson method (without using the
+  %     background grid).
+  %
+  %   RANDPOISSONDISC2D uses the RAND function in its current/existing state.
+  %
   % Ethan Johnson, Copyriht 2016
   %
   % Inspiration:
@@ -47,27 +48,28 @@ function [x,y] = randPoissonDisc2D( r, varargin )
   
   % Add points
   while s>0
-      % First check for size overrun
-      if n==size(l,1), ll=zeros(2*n,2); ll(1:n,:)=l; l=ll; clear ll; end
-      if s==size(q,1), qq=zeros(2*s,2); qq(1:s,:)=q; q=qq; clear qq; end
-      % Then proceed to select point in the grid
-      i = floor(rand*s)+1;
-      % And generate candidate(s)
-      c = false; % is it a candidate?
-      for j=1:k % try at most k times
-          aj = 2*pi*rand; rj = sqrt(rand*t + r2); % uniformly-random annulus
-          xj = q(i,2) + rj*cos(aj); yj = q(i,1) + rj*sin(aj); % around point
-          if ( bx(1)<xj&&xj<bx(2) && by(1)<yj&&yj<by(2) ...
-               && ~tooprox( [yj xj], l(1:n,:), r ) ) % in bounds & not close?
-              n = n+1; l(n,:) = [yj xj]; % save in list
-              s = s+1; q(s,:) = [yj xj]; % add to queue
-              c = true; % signal that candidate was found
-              break; % quit making candidates
-          end
+    % First check for size overrun
+    if n==size(l,1), ll=zeros(2*n,2); ll(1:n,:)=l; l=ll; clear ll; end
+    if s==size(q,1), qq=zeros(2*s,2); qq(1:s,:)=q; q=qq; clear qq; end
+    % Then proceed to select point in the grid
+    i = floor(rand*s)+1;
+    % And generate candidate(s)
+    c = false; % is it a candidate?
+    for j=1:k % try at most k times
+      aj = 2*pi*rand; rj = sqrt(rand*t + r2); % uniformly-random annulus
+      xj = q(i,2) + rj*cos(aj); yj = q(i,1) + rj*sin(aj); % around point
+      if ( bx(1)<xj&&xj<bx(2) && by(1)<yj&&yj<by(2) ...
+        && ~tooprox( [yj xj], l(1:n,:), r ) ) % in bounds & not close?
+
+        n = n+1; l(n,:) = [yj xj]; % save in list
+        s = s+1; q(s,:) = [yj xj]; % add to queue
+        c = true; % signal that candidate was found
+        break; % quit making candidates
       end
-      if ~c, q(i,:) = q(s,:); s = s-1; end % remove point from queue
+    end
+    if ~c, q(i,:) = q(s,:); s = s-1; end % remove point from queue
   end; clear i j aj rj xj yj c;
-  
+
   % Return
   x = l(1:n,2); y = l(1:n,1);
 end
@@ -78,9 +80,9 @@ function tp = tooprox(p,l,e)
   %  + P is the point as an n-tuple (row-vector)
   %  + L is the list of points with n-tuples across and different points down
   %  + E is the exclusion distance (2-norm of P-L)
-  
+
   d = sum( bsxfun(@plus,-p,l).^2, 2);
-  
+
   tp = any( d < (e^2) );
 end
 
