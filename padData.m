fd7a9a8b7eae57d12aff46d210ff6d5388a38aab
Nicholas Dwork <ndwork@hotmail.com>
2019-02-27 17:42:13 -0800

    Minor changes to padData.

--- padData.m
+++ padData.m
@@ -13,6 +13,11 @@ function padded = padData( data, N, varargin )
   % padded - the padded array
   %
   % Written by Nicholas Dwork - Copyright 2016
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
 
   defaultPadValue = 0;
   p = inputParser;
@@ -22,7 +27,7 @@ function padded = padData( data, N, varargin )
 
   sData = size(data);
   tooSmall = max( sData > N );
-  if tooSmall>0, error('Padded size is too small.'); end;
+  if tooSmall>0, error('Padded size is too small.'); end
 
   nDimData = ndims( data );
   if nDimData==2 && min( size(data) ) == 1
@@ -41,23 +46,23 @@ function padded = padData( data, N, varargin )
     end
     nData = numel( data );
 
-    minY = pdFindMinIndx( N, nData );
+    minY = findMinIndx( N, nData );
     padded( minY : minY+nData-1 ) = data;
 
   elseif nDimData == 2
     padded = zeros( N(:)' );
     sData = size( data );
-    minY = pdFindMinIndx( N(1), sData(1) );
-    minX = pdFindMinIndx( N(2), sData(2) );
+    minY = findMinIndx( N(1), sData(1) );
+    minX = findMinIndx( N(2), sData(2) );
     padded( minY : minY+sData(1)-1, ...
             minX : minX+sData(2)-1 ) = data;
 
   elseif nDimData == 3
     padded = zeros( N(:)' );
     sData = size( data );
-    minY = pdFindMinIndx( N(1), sData(1) );
-    minX = pdFindMinIndx( N(2), sData(2) );
-    minZ = pdFindMinIndx( N(3), sData(3) );
+    minY = findMinIndx( N(1), sData(1) );
+    minX = findMinIndx( N(2), sData(2) );
+    minZ = findMinIndx( N(3), sData(3) );
     padded ( minY : minY + sData(1)-1, ...
              minX : minX + sData(2)-1, ...
              minZ : minZ + sData(3)-1  ) = data;
@@ -67,7 +72,7 @@ end
 
 
 
-function minIndx = pdFindMinIndx( N, nData )
+function minIndx = findMinIndx( N, nData )
   if mod(nData,2)==0
     if mod(N,2)==0
       minIndx = (N-nData)/2 + 1;
