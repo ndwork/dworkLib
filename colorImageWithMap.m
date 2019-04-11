2e9d6f5ebf166a11f16a01d96c3b41e96983a1d2
Nicholas Dwork <ndwork@hotmail.com>
2019-04-10 20:44:30 -0700

    Adding colorImageWithMap to repository.

---
+++ colorImageWithMap.m
@@ -0,0 +1,29 @@
+
+function colorImg = colorImageWithMap( img, varargin )
+  % colorImg = colorImageWithMap( img [, cmap ] )
+  %
+  % Inputs:
+  % img - 2D array representing the image
+  %
+  % Optional Inputs:
+  % cmap - the colormap (default is hot)
+  %
+  % Written by Nicholas - Copyright 2019
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  p = inputParser;
+  p.addOptional( 'mapName', 'hot', @(x) true );
+  p.parse( varargin{:} );
+  mapName = p.Results.mapName;
+
+  maxImg = max( img(:) );
+  scaledImg = uint8( img / maxImg * 255 );  %#ok<NASGU>
+
+  colorImg = 0;
+  str2eval = [ 'colorImg = ind2rgb( scaledImg, ', mapName, '(256) );' ];
+  eval( str2eval );
+end
