a1040c8659ef33672e9b7deb6ee962d7ec9b3b1c
Nicholas Dwork <ndwork@hotmail.com>
2019-06-01 15:32:24 -0700

    Adding indx2str to the repository.

---
+++ indx2str.m
@@ -0,0 +1,35 @@
+
+function out = indx2str( indx, N )
+  % out = indx2str( indx, N )
+  %
+  % Converts index to a string the appropriate size for N with padded zeros
+  % For example, if N is 100, will convert indx=88 into '0880';
+  %
+  % Inputs:
+  % indx - the number to convert into a string
+  % N - the maximum indx that will be converted
+  %
+  % Outputs:
+  % out - string
+  %
+  % Example:
+  %
+  % N = 1000;
+  % for i=1:N
+  %   disp([ 'Working on ', indx2str(i,N), ' of ', N ]);
+  %   ... do important stuff here ...
+  % end
+  %
+  % Written by Nicholas - Copyright 2017
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  nDigits = floor( log10( N ) ) + 1;
+
+  formatSpec = [ '%', num2str(nDigits), '.', num2str(nDigits), 'i' ];
+
+  out = num2str( indx, formatSpec );
+end
