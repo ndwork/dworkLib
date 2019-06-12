9aff2378f6452ef94f1bd41593d607e5dce78592
Nicholas Dwork <ndwork@hotmail.com>
2019-06-11 17:05:38 -0700

    Adding Deaubechies wavelet transform to the repository.

---
+++ iwtDeaubechies.m
@@ -0,0 +1,64 @@
+
+function sig = iwtDeaubechies( wt, varargin )
+  % sig = iwtDeaubechies( wt [, split] );
+  % Performs an inverse Deaubechies wavelet transform of a signal
+  % Note that this is a unitary transform
+  %
+  % Inputs:
+  % wt - 1D array representing the wavelet transform of the signal
+  %
+  % Optional Inputs:
+  % split - array specifying the number of levels of the wavelet transform.
+  %   by default, split is 1 (indicating only one level).
+  %   Example: [1 1 1 0] will have 3 levels.  The size of the last portion
+  %   in the final level will be double that of the other portions since it
+  %   wasn't split.
+  %
+  % Written by Nicholas Dwork - Copyright 2019
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  defaultSplit = 1;
+  p = inputParser;
+  p.addOptional( 'split', defaultSplit );
+  p.parse( varargin{:} );
+  split = p.Results.split;
+
+  nwt = numel(wt);
+  wt1 = wt(1:nwt/2);
+  wt2 = wt(nwt/2+1:end);
+
+  nSplit = numel(split);
+  if nSplit > 1
+    split1 = split(1:nSplit/2);
+    split2 = split(nSplit/2+1:end);
+
+    if sum(split1)>0
+      wt1 = iwtDeaubechies( wt1, split1 );
+    end
+    if sum(split2)>0
+      wt2 = iwtDeaubechies( wt2, split2 );
+    end
+  end
+
+  tmp = upsample( wt1, 2 );
+  tmp3 = 3 * tmp;
+  tmpSqrt3 = tmp * sqrt(3);
+  sig1 = tmp - tmpSqrt3 + ...
+    circshift( tmp3 - tmpSqrt3, 1 ) + ...
+    circshift( tmp3 + tmpSqrt3, 2 ) + ...
+    circshift( tmp + tmpSqrt3, 3 );
+  
+  tmp = upsample( wt2, 2 );
+  tmp3 = 3 * tmp;
+  tmpSqrt3 = tmp * sqrt(3);
+  sig2 = -( tmp + tmpSqrt3 ) + ...
+    circshift( tmp3 + tmpSqrt3, 1 ) + ...
+    circshift( -( tmp3 - tmpSqrt3 ), 2 ) + ...
+    circshift( tmp - tmpSqrt3, 3 );
+  
+  sig = ( sig1 + sig2 ) / ( 4 * sqrt(2) );
+end
---
+++ wtDeaubechies.m
@@ -0,0 +1,66 @@
+
+function wt = wtDeaubechies( sig, varargin )
+  % wt = wtDeaubechies( sig [, split] );
+  % Performs a Deaubechies wavelet transform of a signal
+  % (http://wavelets.pybytes.com/wavelet/db2/)
+  % Note that this is a unitary transform
+  %
+  % Inputs:
+  % sig - 1D array representing the signal to be transformed
+  %
+  % Optional Inputs:
+  % split - array specifying the number of levels of the wavelet transform.
+  %   by default, split is 1 (indicating only one level).
+  %   Example: [1 1 1 0] will have 3 levels.  The size of the last portion
+  %   in the final level will be double that of the other portions since it
+  %   wasn't split.
+  %
+  % Written by Nicholas Dwork - Copyright 2019
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  defaultSplit = 1;
+  p = inputParser;
+  p.addOptional( 'split', defaultSplit );
+  p.parse( varargin{:} );
+  split = p.Results.split;
+
+  sigSqrt3 = sig * sqrt(3);
+  sig3 = 3 * sig;
+
+  sigPsigSqrt3 = sig + sigSqrt3;
+  sig3PsigSqrt3 = sig3 + sigSqrt3;
+  sigMsigSqrt3 = sig - sigSqrt3;
+  sig3MsigSqrt3 = sig3 - sigSqrt3;
+  
+  wt1 = ( sigMsigSqrt3 + ...
+    circshift( sig3MsigSqrt3, -1 ) + ...
+    circshift( sig3PsigSqrt3, -2 ) + ...
+    circshift( sigPsigSqrt3, -3 ) ) / ( 4 * sqrt(2) );
+  wt1 = wt1(1:2:end);
+
+  wt2 = ( -sigPsigSqrt3 + ...
+    circshift( sig3PsigSqrt3, -1 ) + ...
+    circshift( -sig3MsigSqrt3, -2 ) + ...
+    circshift( sigMsigSqrt3, -3 ) ) / ( 4 * sqrt(2) );
+  wt2 = wt2(1:2:end);
+
+  nSplit = numel(split);
+  if nSplit > 1
+    split1 = split(1:nSplit/2);
+    split2 = split(nSplit/2+1:end);
+
+    if sum(split1)>0
+      wt1 = wtDeaubechies( wt1, split1 );
+    end    
+    if sum(split2)>0
+      wt2 = wtDeaubechies( wt2, split2 );
+    end
+  end
+
+  wt = [wt1; wt2;];
+end
+
