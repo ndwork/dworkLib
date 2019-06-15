9a1eed5d1a3beb277b52941e95773b40246448ca
Nicholas Dwork <ndwork@hotmail.com>
2019-06-15 13:49:33 -0700

    Adding binarySearch to the repository.

---
+++ binarySearch.m
@@ -0,0 +1,50 @@
+
+
+function out = binarySearch( f, LB, UB, varargin )
+  % out = binarySearch( f, LB, UB [, 'tol', tol, 'nMax', nMax ] )
+  %
+  % finds the root of the function f using a binary search
+  %
+  % Inputs:
+  % f - function handle
+  % LB - the lower bound of the root
+  % UB - the upper bound of the root
+  %
+  % Optional Inputs:
+  % tol - the tolerance to use when finding the root (default is 1d-6)
+  % nMax - the maximum number of iterations (default is 1000)
+  %
+  % Written by Nicholas Dwork, Copyright 2019
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  p = inputParser;
+  p.addParameter( 'nMax', 1000, @ispositive );
+  p.addParameter( 'tol', 1d-6, @ispositive );
+  p.parse( varargin{:} );
+  nMax = p.Results.nMax;
+  tol = p.Results.tol;
+
+  thisLB = LB;  fLB = f( thisLB );
+  thisUB = UB;  fUB = f( thisUB );
+
+  for i = 1 : nMax
+    mid = 0.5 * ( thisUB + thisLB );
+    if abs( thisUB - thisLB ) < tol, break; end
+
+    fMid = f( mid );
+    if fMid == 0, break; end
+
+    if sign( fLB ) == sign( fMid )
+      thisLB = mid;
+    else
+      thisUB = mid;
+    end
+  end
+
+  out = mid;
+end
+
