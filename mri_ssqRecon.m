10a57198cc3dd67515dd31b0683e51ecb74b95b8
Nicholas Dwork <ndwork@hotmail.com>
2019-08-08 16:32:38 -0700

    A simple sum-of-squares multi-coil reconstruction.

--- mri_ssqRecon.m
+++ mri_ssqRecon.m
@@ -1,29 +1,26 @@
 
-function imgData = mri_ssqRecon( kData )
+function recon = mri_ssqRecon( kData )
   % Perform a sum of squared reconstruction
   %
   % Inputs:
-  % kData is an array of size (Ny,Nx,nSlices,nCoils) of kSpace values
+  % kData is an array of size ( Ny, Nx, nSlices, ..., nCoils ) of kSpace values
   %
   % Output:
   % recon is the reconstructed image
   %
   % Written by Nicholas Dwork - Copyright 2018
   %
+  % https://github.com/ndwork/dworkLib.git
+  %
   % This software is offered under the GNU General Public License 3.0.  It
   % is offered without any warranty expressed or implied, including the
   % implied warranties of merchantability or fitness for a particular
   % purpose.
 
-  [Ny,Nx,nSlices,nCoils] = size( kData );
-  imgData = zeros( Ny, Nx, nSlices );
-  for slice=1:nSlices
-    tmpReconSq = zeros( Ny, Nx );
-    for coil=1:nCoils
-      tmpFFT = squeeze( kData(:,:,slice,phase,coil) );
-      tmpImg = fftshift( ifft2( ifftshift(tmpFFT) ) );
-      tmpReconSq = tmpReconSq + conj(tmpImg) .* tmpImg;
-    end
-    imgData(:,:,slice,phase) = sqrt( tmpReconSq );
-  end
+  coilRecons = mri_fftRecon( kData );
+
+  tmpReconSq = coilRecons .* conj( coilRecons );
+  
+  recon = sqrt( sum( tmpReconSq, ndims(kData) ) );
+
 end
