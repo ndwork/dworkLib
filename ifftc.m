c4770ed16e60549c7ecdb0882beea99ee6c3596d
Nicholas Dwork <ndwork@hotmail.com>
2019-08-08 16:16:09 -0700

    Fixed ifftshift in ifftc.

--- ifftc.m
+++ ifftc.m
@@ -1,13 +1,20 @@
 
 function out = ifftc( in, varargin )
-  % out = ifftc( in, varargin )
+  % out = ifftc( in [, n, dim, true/false ] )
   %
-  % Compute the cenetered inverse fft
+  % Compute the centered ifft
   %
   % Inputs:
   % in - an array (of any number of dimensions)
   %
+  % Optional Inputs:
+  % n - zero pads to this number of elements and then perform an ifft
+  %     if a two dimensional ifft, the zero pads to size [n(1) n(2)] and performs ifft
+  % dim - if supplied, only does an ifft along this dimension
+  %
   % Written by Nicholas Dwork, Copyright 2019
+  %
+  % https://github.com/ndwork/dworkLib.git
   %
   % This software is offered under the GNU General Public License 3.0.  It
   % is offered without any warranty expressed or implied, including the
@@ -15,20 +22,41 @@ function out = ifftc( in, varargin )
   % purpose.
 
   p = inputParser;
-  p.addOptional( 'type', [], @(x) true )
+  p.addOptional( 'n', [], @(x) numel(x) == 0 || ispositive(x) );
+  p.addOptional( 'dim', [], @isnumeric );
+  p.addOptional( 'unitary', false, @(x) islogical(x) || isnumeric(x) );
   p.parse( varargin{:} );
-  type = p.Results.type;
+  dim = p.Results.dim;
+  n = p.Results.n;
+  unitary = p.Results.unitary;
 
-  switch ndims( in )
-    case 1
-      out = ifft( ifftshift( in ) );
-    case 2
-      out = ifft2( ifftshift( in ) );
-    otherwise
-      out = ifftn( ifftshift( in ) );
+  if numel( dim ) > 0
+    out = ifft( ifftshift( in, dim ), n, dim );
+
+  else
+
+    switch ndims( in )
+      case 1
+        out = ifft( ifftshift( in ), n );
+
+      case 2
+        if numel( n ) > 0
+          if numel( n ) ~= 2, error( 'sz has the wrong number of elements' ); end
+          out = ifft2( ifftshift( in ), n(1), n(2) );
+        else
+          out = ifft2( ifftshift( in ) );
+        end
+
+      otherwise
+        if numel( n ) ~= 0 && numel( n ) ~= ndims( in )
+          error( 'n has the wrong number of elements' );
+        end
+        out = ifftn( ifftshift( in ), n );
+    end
+
   end
 
-  if strcmp( type, 'unitary' )
+  if unitary == true
     out = out / sqrt( numel(in) );
   end
 
