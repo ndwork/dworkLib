34142eb982ec7ea9b3caddf8881344853476409a
Nicholas Dwork <ndwork@hotmail.com>
2019-08-18 11:46:06 -0700

    Added comments to restart and pressShift.

--- pressShift.m
+++ pressShift.m
@@ -1,6 +1,20 @@
 
 
 function pressShift
+  % pressShift
+  %
+  % Uses the java robot to press and depress shift.
+  % Called in startup so that restart exits debug mode correctly.
+  %
+  % Written by Nicholas Dwork - Copyright 2019
+  %
+  % https://github.com/ndwork/dworkLib.git
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
   import java.awt.Robot;
   import java.awt.event.KeyEvent;
 
--- restart.m
+++ restart.m
@@ -11,6 +11,8 @@ function restart( varargin )
   %
   % Written by Nicholas Dwork - Copyright 2017
   %
+  % https://github.com/ndwork/dworkLib.git
+  %
   % This software is offered under the GNU General Public License 3.0.  It
   % is offered without any warranty expressed or implied, including the
   % implied warranties of merchantability or fitness for a particular
