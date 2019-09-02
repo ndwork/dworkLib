d8fb1bd3f1ef8fee68f25dad1511eadd7f721f6a
Nicholas Dwork <ndwork@hotmail.com>
2019-09-01 21:05:07 -0700

    Adding getSubFeaturesFromImg to the repository.

---
+++ getSubFeaturesFromImg.m
@@ -0,0 +1,60 @@
+
+function [features,values] = getSubFeaturesFromImg( img, n, varargin )
+  % features = getSubFeaturesFromImg( img, n [, scale ] )
+  %
+  % Inputs:
+  % n - the number of features to select
+  % scale - the scale of the displayed image (default is 1)
+  %
+  % Output:
+  % features - a 2 column array.  The first column are the x (or
+  % horizontal) coordinates, and the second column are the y (or
+  % vertical coordinates).
+  %
+  % Optional Output:
+  % values - the values of the image at each feature point
+  %
+  % Written by Nicholas Dwork - Copyright 2019
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  p = inputParser;
+  p.addRequired( 'img', @(x) isnumeric(x) && ismatrix(x) );
+  p.addRequired( 'n', @(x) isnumeric(x) && x > 0 );
+  p.addOptional( 'scale', 1, @isnumeric );
+  p.parse( img, n, varargin{:} );
+  scale = p.Results.scale;
+
+  mainF = figure;
+  imshowscale( img, scale );
+
+  features = zeros( n, 2 );
+  subF = figure;
+  for pt = 1 : n
+    figure( mainF );
+    grossPt = round( ginput( 1 ) / scale );
+    hL = max( grossPt(1) - 10, 1 );
+    vL = max( grossPt(2) - 10, 1 );
+    hU = min( grossPt(1) + 10, size(img,2) );
+    vU = min( grossPt(2) + 10, size(img,1) );
+    subImg = img( vL : vU, hL : hU, : );
+    figure( subF );
+    imshowscale( subImg, scale*10 );
+    features(pt,:) = double( ginput(1) ) / ( scale * 10 );
+    features(pt,:) = features(pt,:) + [ hL-1 vL-1 ];
+  end
+  close( subF );
+  close( mainF );
+
+
+  if nargout > 1
+    values = zeros( n, size(img,3) );
+    roundedFeatures = round( features );
+    for fIndx = 1:n
+      values(fIndx,:) = img( roundedFeatures(fIndx,2), roundedFeatures(fIndx,1), : );
+    end
+  end
+end
