e86ec3485648f8c7b3bec84af1cd9f02bd1f6984
Nicholas Dwork <ndwork@hotmail.com>
2019-09-04 18:09:04 -0700

    Added ability to impose phase constraint when estimating T1.

--- mri_mapT1InversionRecovery.m
+++ mri_mapT1InversionRecovery.m
@@ -1,8 +1,10 @@
 
-
 function t1Map = mri_mapT1InversionRecovery( dataCube, TIs, varargin )
   % t1Map = mri_mapT1InversionRecovery( dataCube, TIs, ...
   %   [, 'mask', mask, 'verbose', verbose ] )
+  %
+  % This method is implemented according to equation 6 of "A Robust Methodology
+  % for In Vivo T1 Mapping" by Barral et al.
   %
   % Inputs:
   % dataCube - a 3D array of size MxNxK.  Each k index corresponds to an
@@ -30,8 +32,10 @@ function t1Map = mri_mapT1InversionRecovery( dataCube, TIs, varargin )
   mask = p.Results.mask;
   verbose = p.Results.verbose;
 
+  usePhaseConstraint = false;
+
   sData = size( dataCube );
-  if numel( mask ) == 0, mask=ones( sData(1:2) ); end;
+  if numel( mask ) == 0, mask=ones( sData(1:2) ); end
   %showImageCube( mask.*abs(dataCube), showScale );  titlenice('t1 ir Data');
 
   fminconOptions = optimoptions('fmincon','Display','off');
@@ -39,17 +43,30 @@ function t1Map = mri_mapT1InversionRecovery( dataCube, TIs, varargin )
   t1MapCols = cell( sData(2), 1 );
   p = parforProgress( sData(2) );
   parfor i=1:sData(2)
-    if verbose ~= 0, p.progress( i, 10 );  end;                                            %#ok<PFBNS>
-    t1MapCol = zeros( sData(1), 1 );                                                  %#ok<PFBNS>
+    if verbose ~= 0, p.progress( i, 10 ); end   %#ok<PFBNS>
+    t1MapCol = zeros( sData(1), 1 );   %#ok<PFBNS>
 
     for j=1:sData(1)
-      if mask(j,i) == 0, continue; end;
+      if mask(j,i) == 0, continue; end
+
       thisData = squeeze( dataCube(j,i,:) ); 
-      [~,minSigIndx] = min(abs(thisData));
-      tmp0 = [ 1; 0; 1; 0; TIs(minSigIndx) ];                                              %#ok<PFBNS>
-      tmp = fmincon( @(tmp) norm( thisData - f(tmp,TIs(:)) ), ...
-        tmp0, [],[],[],[], [-Inf;-Inf;-Inf;-Inf;0], [], [], fminconOptions );
+      [~,minSigIndx] = min( abs( thisData ) );
+      tmp0 = [ 1; 0; 1; 0; TIs(minSigIndx) ];   %#ok<PFBNS>
+      if usePhaseConstraint == true
+        tmp = fmincon( @(tmp) norm( thisData - f(tmp,TIs(:)) ), tmp0, [], [], ...
+         [],[], [-Inf;-Inf;-Inf;-Inf;0], [], @phaseConstraint, fminconOptions );
+      else
+        tmp = fmincon( @(tmp) norm( thisData - f(tmp,TIs(:)) ), tmp0, [], [], ...
+         [],[], [-Inf;-Inf;-Inf;-Inf;0], [], [], fminconOptions );
+      end
       t1MapCol(j) = tmp(5);
+
+      figure; plotnice( TIs, real(thisData) );
+      hold all;  plotnice( TIs, real(f(tmp,TIs(:))) );  titlenice( 'real fit' );
+      legendnice( 'data', 'model' )
+      figure; plotnice( TIs, imag(thisData) );
+      hold all;  plotnice( TIs, imag(f(tmp,TIs(:))) );  titlenice( 'imag fit' );
+      legendnice( 'data', 'model' )
     end
     t1MapCols{i} = t1MapCol;
   end
@@ -60,4 +77,11 @@ function t1Map = mri_mapT1InversionRecovery( dataCube, TIs, varargin )
   end
 
 end
+
+
+function [c,ceq] = phaseConstraint( params )
+  c = abs( angle( params(1) + 1i * params(2) ) - angle( params(3) + 1i * params(4) ) );
+  ceq = zeros(numel(c),1);
+end
+  
 
