80fcef964b995d9d4056104026481189745e28fb
Nicholas Dwork <ndwork@hotmail.com>
2019-09-23 18:01:42 -0700

    Adding objectiveValues output in chambollePockWLS.

--- chambollePockWLS.m
+++ chambollePockWLS.m
@@ -28,7 +28,8 @@ function [xStar,objValues] = chambollePockWLS( x, proxf, proxgConj, varargin )
   p = inputParser;
   p.addParameter( 'A', [] );
   p.addParameter( 'beta', 1, @ispositive );
-  p.addParameter( 'delta', 0.95, @(x) x>0 && x<1 );
+  p.addParameter( 'delta', 0.99, @(x) x>0 && x<1 );
+  p.addParameter( 'doCheckAdjoint', false, @islogical );
   p.addParameter( 'f', [] );
   p.addParameter( 'g', [] );
   p.addParameter( 'mu', 0.7, @(x) x>0 && x<1 );
@@ -40,6 +41,7 @@ function [xStar,objValues] = chambollePockWLS( x, proxf, proxgConj, varargin )
   A = p.Results.A;
   beta = p.Results.beta;
   delta = p.Results.delta;
+  doCheckAdjoint = p.Results.doCheckAdjoint;
   f = p.Results.f;
   g = p.Results.g;
   mu = p.Results.mu;
@@ -51,37 +53,51 @@ function [xStar,objValues] = chambollePockWLS( x, proxf, proxgConj, varargin )
   if numel( A ) == 0
     applyA = @(x) x;
     applyAT = @(x) x;
+    y = x;
   elseif isnumeric( A )
     applyA = @(x) A * x;
     applyAT = @(y) A' * y;
+    y = A * x;
   else
     applyA = @(x) A( x, 'notransp' );
     applyAT = @(x) A( x, 'transp' );
+    y = applyA( x );
+  end
+
+  if doCheckAdjoint == true
+    [adjointCheckPassed,adjCheckErr] = checkAdjoint( x, applyA, applyAT );
+    if ~adjointCheckPassed, error([ 'checkAdjoint failed with error ', num2str(adjCheckErr) ]); end
   end
 
   if nargout > 1,  objValues = zeros( N, 1 ); end
 
-  y = applyA( x );
   for optIter = 1 : N
     if verbose == true
       disp([ 'chambollePockWLS: working on ', num2str(optIter), ' of ', num2str(N) ]);
     end
 
     lastX = x;
-    tmp = x - tau * applyAT( y );
+    tmp = lastX - tau * applyAT( y );
     x = proxf( tmp, tau );
+
+    if nargout > 1
+      objValues( optIter ) = f( x ) + g( applyA( x ) );
+    end
 
     lastTau = tau;
     tau = tau * sqrt( 1 + theta );
+
     diffx = x - lastX;
+
     lastY = y;
     while true
       theta = tau / lastTau;
 
       xBar = x + theta * ( diffx );
 
-      tmp = y + beta * tau * applyA( xBar );
-      y = proxgConj( tmp, beta * tau );
+      betaTau = beta * tau;
+      tmp = lastY + betaTau * applyA( xBar );
+      y = proxgConj( tmp, betaTau );
 
       diffy = y - lastY;
       ATdiffy = applyAT( diffy );
