23b7052dd72ed55f9e5e61149de83285975f9ae8
Nicholas Dwork <ndwork@hotmail.com>
2019-10-16 18:38:15 -0700

    Adding nucNorm to the repository.

---
+++ nucNorm.m
@@ -0,0 +1,21 @@
+
+function out = nucNorm( in )
+  % out = nucNorm( in )
+  %
+  % Computes the nuclear norm of the input.  The nuclear norm is the input
+  % of the singular values of the matrix.
+  %
+  % Written by Nicholas Dwork - Copyright 2019
+  %
+  % https://github.com/ndwork/dworkLib.git
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  [~,s,~] = svd( in, 'econ' );
+  out = sum( diag( s ) );
+end
+
+
