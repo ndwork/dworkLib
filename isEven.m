1edadc7320608425f85150e3b5cb3070213cc62f
Nicholas Dwork <ndwork@hotmail.com>
2019-10-16 18:38:04 -0700

    Adding comments to isEven.

--- isEven.m
+++ isEven.m
@@ -1,14 +1,23 @@
 
 function [out,error] = isEven( data, varargin )
   % out = isEven( data, [ threshold ] )
-  % Determines whether or not data is even (with circular boundary
-  %   conditions)
+  %
+  % Determines whether or not data is even (with circular boundary conditions)
   % Indexes are defined according to fftshift
   %
   % Inputs:
   % data is a 1D or 2D array
   % threshold is an optional input.  The relative error between data(x)
   %   and data(-x) must be less than threshold to be considered even.
+  %
+  % Written by Nicholas Dwork - Copyright 2016
+  %
+  % https://github.com/ndwork/dworkLib.git
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
 
   defaultThresh = 0;
   p = inputParser;
