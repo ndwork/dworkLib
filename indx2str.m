87e5604648cc6e9f4f5a001b116dd96f544dd69e
Nicholas Dwork <ndwork@hotmail.com>
2019-12-28 19:38:29 -0800

    Adding usage to indx2str.

--- indx2str.m
+++ indx2str.m
@@ -20,12 +20,14 @@ function out = indx2str( indx, N )
   %   ... do important stuff here ...
   % end
   %
-  % Written by Nicholas - Copyright 2017
+  % Written by Nicholas - Copyright 2019
   %
   % This software is offered under the GNU General Public License 3.0.  It
   % is offered without any warranty expressed or implied, including the
   % implied warranties of merchantability or fitness for a particular
   % purpose.
+
+  if nargin < 2, disp( 'out = indx2str( indx, N )' ); end
 
   nDigits = floor( log10( N ) ) + 1;
 
