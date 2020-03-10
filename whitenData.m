b84f317b007e7787327cdfecd8d6b12c8d7ffe4c
Nicholas Dwork <ndwork@hotmail.com>
2020-03-09 18:09:02 -0700

    Added PCA whitening type to whitenData.

--- whitenData.m
+++ whitenData.m
@@ -1,6 +1,6 @@
 
-function out = whitenData( data, covMatrix )
-  % out = whitenData( data, covMatrix )
+function out = whitenData( data, covMatrix, varargin )
+  % out = whitenData( data, covMatrix [, type ] )
   %
   % Assumes data is zero mean
   %
@@ -8,6 +8,10 @@ function out = whitenData( data, covMatrix )
   % data - a multidimensional array; it is assumed that the last dimension
   %   is the random vector dimension
   % covMatrix - a covariance matrix
+  %
+  % Optional Inputs:
+  % type - the type of whitening to perform.
+  %   'chol' for Cholesky or 'pca' for Principal Component Analysis
   %
   % Outputs:
   % out - an array of whitened vectors of size data
@@ -20,20 +24,37 @@ function out = whitenData( data, covMatrix )
   % purpose.
 
   if nargin < 2
-    disp( 'Usage:  out = whitenData( data, covMatrix )' );
+    disp( 'Usage:  out = whitenData( data, covMatrix [, type ] )' );
     return
   end
+
+  p = inputParser;
+  p.addOptional( 'type', 'chol', @(x) true );
+  p.parse( varargin{:} );
+  type = p.Results.type;
 
   if size( data, ndims( data ) ) ~= size( covMatrix, 1 ) || ...
      size( data, ndims( data ) ) ~= size( covMatrix, 2 )
      error( 'Dimension mismatch' );
   end
 
-  L = chol( covMatrix, 'lower' );
-
   sData = size( data );
   reshaped = transpose( reshape( data, [ prod( sData(1:end-1) ) sData(end) ] ) );
-  tmp = L \ reshaped;
+  
+  if strcmp( type, 'chol' )
+    L = chol( covMatrix, 'lower' );
+    whitened = L \ reshaped;
 
-  out = reshape( tmp, sData );
+  elseif strcmp( type, 'pca' )
+    % PCA whitening
+    %  https://stats.stackexchange.com/questions/95806/how-to-whiten-the-data-using-principal-component-analysis
+    [u,s,~] = svd( covMatrix, 'econ' );
+    whitened = sqrt(s) \ u' * reshaped;
+
+  else
+    error( 'Unknown whitening type' );
+
+  end
+
+  out = reshape( whitened, sData );
 end
