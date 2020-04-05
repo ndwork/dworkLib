760b7b54986b4109e55b35d3d5d9a6ba0888192d
Nicholas Dwork <ndwork@hotmail.com>
2020-04-05 07:47:35 -0700

    Added git url to smoothImg.

--- smoothImg.m
+++ smoothImg.m
@@ -17,6 +17,8 @@ function out = smoothImg( in, varargin )
   %
   % Written by Nicholas - Copyright 2019
   %
+  % https://github.com/ndwork/dworkLib.git
+  %
   % This software is offered under the GNU General Public License 3.0.  It
   % is offered without any warranty expressed or implied, including the
   % implied warranties of merchantability or fitness for a particular
