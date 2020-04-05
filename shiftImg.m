85c867b1d0431e5ff608a6ec0eb659069646b97b
Nicholas Dwork <ndwork@hotmail.com>
2020-04-05 07:47:09 -0700

    Added git url to shiftImg.

--- shiftImg.m
+++ shiftImg.m
@@ -1,7 +1,3 @@
-% This software is offered under the GNU General Public License 3.0.  It 
-% is offered without any warranty expressed or implied, including the 
-% implied warranties of merchantability or fitness for a particular 
-% purpose.
 
 function out = shiftImg( img, shifts )
   % out = shiftImg( img, shifts )
@@ -17,6 +13,8 @@ function out = shiftImg( img, shifts )
   % out - the shifted image
   %
   % Written by Nicholas Dwork - Copyright 2016
+  %
+  % https://github.com/ndwork/dworkLib.git
   %
   % This software is offered under the GNU General Public License 3.0.  It
   % is offered without any warranty expressed or implied, including the
