3d30be06cb8b31cea8bfcd9a88ff26c0cec4d8a6
Nicholas Dwork <ndwork@hotmail.com>
2020-04-21 13:18:01 -0700

    findFractionAboveValue now uses dworkLib's binary search.

--- findFractionAboveValue.m
+++ findFractionAboveValue.m
@@ -22,23 +22,6 @@ function value = findFractionAboveValue( data, fraction )
   lowerValue = min( data(:) );
   upperValue = max( data(:) );
 
-  fractionAbove = 0;
-  lastFractionAbove = fractionAbove - 1.0;
-
-  % Conduct a binary search
-  while( lastFractionAbove ~= fractionAbove )
-    lastFractionAbove = fractionAbove;
-
-    value = lowerValue + 0.5 * ( upperValue - lowerValue );
-    fractionAbove = sum( data(:) > value ) / nData;
-
-    if fractionAbove == fraction
-      return;
-    elseif fractionAbove > fraction
-      lowerValue = value;
-    else
-      upperValue = value;
-    end
-  end
-
+  f = @( value ) fraction - sum( data(:) >= value ) / nData; 
+  value = binarySearch( f, lowerValue, upperValue, 'tol', 1d-8, 'nMax', Inf );
 end
