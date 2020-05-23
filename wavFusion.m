6f397c8de0ed34c9ef93b463a8defed1de75cf37
Nicholas Dwork <ndwork@hotmail.com>
2020-05-22 18:25:44 -0700

    Adding wavFusion to the repository.

---
+++ wavFusion.m
@@ -0,0 +1,39 @@
+
+function fused = wavFusion( cImg, mImg )
+  % fused = wavFusion( cImg, mImg )
+  %
+  % Inputs:
+  % cImg - color or monochrome image
+  % mImg - monochrome image
+  %
+  % Written by Nicholas Dwork - Copyright 2016
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  split = zeros( 4 );
+  split(1,1) = 1;
+
+  sImg = size( cImg );
+  nChannels = size( cImg, 3 );
+  wtColor = zeros( sImg );
+  for i = 1 : nChannels
+    wtColor(:,:,i) = wtDeaubechies2( cImg(:,:,i), split );
+  end
+
+  wtMono = wtDeaubechies2( mImg, split );
+
+  fused = zeros( sImg );
+  activityMono = abs(wtMono);
+  activityColor = abs(wtColor);
+  for i = 1 : nChannels
+    wtFused = wtColor(:,:,i);
+    wtFused( activityMono > activityColor(:,:,i) ) = ...
+      wtMono( activityMono > activityColor(:,:,i) );
+    fused(:,:,i) = iwtDeaubechies2( wtFused, split );
+  end
+
+  fused = min( max( fused, 0 ), 1 );
+end
