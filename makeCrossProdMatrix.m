7c18991f899f1126c4cf4747cb1e7ce30f3c010c
Nicholas Dwork <ndwork@hotmail.com>
2020-06-13 18:31:26 -0700

    Added comments to makeCrossProdMatrix.

--- makeCrossProdMatrix.m
+++ makeCrossProdMatrix.m
@@ -2,6 +2,8 @@
 function out = makeCrossProdMatrix( v )
   % out = makeCrossProdMatrix( v )
   %
+  % makes the cross product matrix out so that cross(v,u) = out * u
+  %
   % Inputs:
   % v - a 3 elements array
   %
@@ -9,6 +11,11 @@ function out = makeCrossProdMatrix( v )
   % out - a 3x3 matrix
   %
   % Written by Nicholas Dwork - Copyright 2016
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
   
   out = zeros(3);
   out(1,2) = -v(3);
