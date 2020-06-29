08d67233de8435d93c59ad5fcc0f64901e9e001e
Nicholas Dwork <ndwork@hotmail.com>
2020-06-28 17:13:18 -0700

    Removing restart_all and recombining the functionality into restart.

--- restart.m
+++ restart.m
@@ -1,33 +1,52 @@
 
-% restart
-%
-% closes all windows, clears variables, clears the command window, ends debug
-% mode, and deletes any parforProgess files that were created by the current
-% instance of Matlab
-%
-% Written by Nicholas Dwork - Copyright 2017
-%
-% https://github.com/ndwork/dworkLib.git
-%
-% This software is offered under the GNU General Public License 3.0.  It
-% is offered without any warranty expressed or implied, including the
-% implied warranties of merchantability or fitness for a particular
-% purpose.
+function restart( varargin )
+  % restart
+  %
+  % closes all windows, clears variables, clears the command window, ends debug
+  % mode, and deletes any parforProgess files that were created by the current
+  % instance of Matlab
+  %
+  % Written by Nicholas Dwork - Copyright 2017
+  %
+  % https://github.com/ndwork/dworkLib.git
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
 
+  p = inputParser;
+  p.addOptional( 'type', [], @(x) true );
+  p.parse( varargin{:} );
+  type = p.Results.type;
 
-pid = feature('getpid');
-parforProgressFile = ['parforProgress_', num2str(pid), '.txt'];
-if exist( parforProgressFile, 'file' )
-  delete( parforProgressFile );
+  if strcmp( type, 'all' )
+    evalin( 'base', 'clear all' );  % delete all variables in global scope
+    evalin( 'caller', 'clear all' );  % delete all variables in global scope
+    clear all;  %#ok<CLALL>
+
+    parforFiles = dir( 'parforProgress*.txt' );
+    for fileIndx = 1 : numel( parforFiles )
+      delete( parforFiles(fileIndx).name );
+    end
+
+  else
+    pid = feature('getpid');
+    parforProgressFile = ['parforProgress_', num2str(pid), '.txt'];
+    if exist( parforProgressFile, 'file' )
+      delete( parforProgressFile );
+    end
+
+  end
+
+  close all;  clc;  clear;  clear global;
+
+  % I would like to be able to call 'dbquit' here, but Matlab
+  % prevents it.  So instead, I must simulate keyboard commands,
+  % which is what is done here:
+  pressShiftF5;
+  evalin( 'base', 'clear all' );
 end
-
-close all;  clc;  clear;  clear global;
-
-% I would like to be able to call 'dbquit' here, but Matlab
-% prevents it.  So instead, I must simulate keyboard commands,
-% which is what is done here:
-pressShiftF5;
-evalin( 'base', 'clear all' );
 
 
 function pressShiftF5
