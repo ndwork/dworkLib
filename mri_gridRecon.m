33ba3ac89136f1c0324a770bd1b12a7cb3936dd3
Nicholas Dwork <ndwork@hotmail.com>
2020-09-23 12:43:49 -0700

    Adding mri_gridRecon to the repository.

---
+++ mri_gridRecon.m
@@ -0,0 +1,57 @@
+
+function out = mri_gridRecon( data, traj, sImg, varargin )
+  % out = mri_gridRecon( data, traj, sImg [, 'alpha', alpha, 'W', W, 'nC', nC, ...
+  %   'weights', weights ] )
+  %
+  % data - an NxC array of data values where C is the number of
+  %   coils
+  %
+  % Outputs:
+  % out - an sImg x C array that is the set of reconstructions
+  %
+  % Written by Nicholas Dwork, Copyright 2020
+  %
+  % https://github.com/ndwork/dworkLib.git
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  defaultAlpha = 1.5;
+  defaultW = 8;
+  defaultNc = 500;
+
+  checknum = @(x) isnumeric(x) && isscalar(x) && (x >= 1);
+  p = inputParser;
+  p.addParameter( 'alpha', defaultAlpha, @(x) numel(x) == 0 || checknum(x) );
+  p.addParameter( 'W', defaultW, @(x) numel(x) == 0 || checknum(x) );
+  p.addParameter( 'nC', defaultNc, @(x) numel(x) == 0 || checknum(x) );
+  p.addParameter( 'weights', [], @isnumeric );
+  p.parse( varargin{:} );
+  alpha = p.Results.alpha;
+  W = p.Results.W;
+  nC = p.Results.nC;
+  weights = p.Results.weights;
+  
+  if numel( alpha ) == 0, alpha = defaultAlpha; end
+  if numel( W ) == 0, W = defaultW; end
+  if numel( nC ) == 0, nC = defaultNc; end
+
+  nCoils = size( data, 2 );
+
+  if numel( weights ) == 0
+    weights = makePrecompWeights_2D( traj, sImg, ...
+      'alpha', alpha, 'W', W, 'nC', nC );
+  end
+
+  coilRecons = cell( 1, 1, nCoils );
+  parfor gCoilIndx = 1 : nCoils
+    coilData = data(:,gCoilIndx);
+    thisRecon = grid_2D( coilData(:), traj, sImg, weights, ...
+      'alpha', alpha, 'W', W, 'nC', nC );
+    coilRecons{1,1,gCoilIndx} = thisRecon;
+  end
+  out = cell2mat( coilRecons );
+
+end
