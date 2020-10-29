8fa42d44fe2799cccca11997418ebbd4ec0a10b9
Nicholas Dwork <ndwork@hotmail.com>
2020-10-28 22:47:58 -0700

    Adding proxL2 to the repository.

---
+++ proxL2.m
@@ -0,0 +1,26 @@
+
+
+function out = proxL2( x, t )
+  % out = proxL2( x, t )
+  %
+  % Calculates the proximal operator of f(x) = t * L2( x )
+  %
+  % Inputs:
+  % x - a 1D array
+  % t - a scalar
+  %
+  % Written by Nicholas Dwork - Copyright 2020
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular purpose.
+
+  nx = norm( x(:) );
+
+  if nx <= t
+    out = 0;
+    return;
+  end
+
+  out = ( 1 - t / nx ) * x;
+end
