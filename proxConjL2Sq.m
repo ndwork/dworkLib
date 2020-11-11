91afff64eea16367e283d8bd6acf5504842e2d26
Nicholas Dwork <ndwork@hotmail.com>
2020-11-10 17:43:13 -0800

    Removed a division and made it multiplication.

--- proxConjL2Sq.m
+++ proxConjL2Sq.m
@@ -18,6 +18,6 @@ function out = proxConjL2Sq( x, sigma, c, b )
   if nargin < 3, c=1; end
   if nargin < 4, b=0; end
 
-  out = ( x - sigma * b ) / ( sigma / c + 1 );
+  out = c * ( x - sigma * b ) / ( sigma + c );
 
 end
