26d8e62a6259b7155bb96af8af56969b5af80d59
Nicholas Dwork <ndwork@hotmail.com>
2021-02-03 10:43:59 -0800

    Making mri_gridRecon more general.

--- mri_gridRecon.m
+++ mri_gridRecon.m
@@ -1,17 +1,22 @@
 
-function out = mri_gridRecon( data, traj, sImg, varargin )
-  % out = mri_gridRecon( data, traj, sImg [, 'alpha', alpha, 'W', W, 'nC', nC, ...
-  %   'weights', weights ] )
+function recons = mri_gridRecon( kData, trajs, sImg, varargin )
+  % recons = mri_gridRecon( kData, trajs, sImg [, 'alpha', alpha, 'W', W, 'nC', nC, ...
+  %   'verbose', verbose ] )
   %
-  % data - an NxC array of data values where C is the number of
-  %   coils
+  % Performs a gridding recon for each coil
   %
-  % Outputs:
-  % out - an sImg x C array that is the set of reconstructions
+  % Inputs:
+  % data - Several options:
+  %   A complex N x D1 x D2 x ... x DN array of data values
+  % trajs - a vector of N x 2 x D1 x D2 x ... x DN specifying the k-space trajectory
+  %   If trajs is size N x 2, it is assumed the same trajectory was used for all
+  %   other dimensions.
+  % sImg - the size of the output image
   %
-  % Written by Nicholas Dwork, Copyright 2020
+  % Output:
+  % recons is an array of reconstructed images of size sImg(1) x sImg(2) x D1 x D2 x ... x DN
   %
-  % https://github.com/ndwork/dworkLib.git
+  % Written by Nicholas Dwork - Copyright 2018
   %
   % This software is offered under the GNU General Public License 3.0.  It
   % is offered without any warranty expressed or implied, including the
@@ -19,47 +24,50 @@ function out = mri_gridRecon( data, traj, sImg, varargin )
   % purpose.
 
   if nargin < 1
-    disp( 'Usage:   out = mri_gridRecon( data, traj, sImg [, ''alpha'', alpha, ' );
-    disp( '           ''W'', W, ''nC'', nC, ''weights'', weights ' );
-    return
+    disp( 'Usage:  recons = mri_gridRecon( kData, trajs, sImg [, ''alpha'', alpha, ...' );
+    disp( '          ''W'', W, ''nC'', nC, ''verbose'', verbose ' );
+    return;
   end
-  
-  defaultAlpha = 1.5;
-  defaultW = 8;
-  defaultNc = 500;
 
-  checknum = @(x) isnumeric(x) && isscalar(x) && (x >= 1);
   p = inputParser;
-  p.addParameter( 'alpha', defaultAlpha, @(x) numel(x) == 0 || checknum(x) );
-  p.addParameter( 'W', defaultW, @(x) numel(x) == 0 || checknum(x) );
-  p.addParameter( 'nC', defaultNc, @(x) numel(x) == 0 || checknum(x) );
-  p.addParameter( 'weights', [], @isnumeric );
-  p.addParameter( 'verbose', false, @(x) islogical(x) || isnumeric(x) );
+  p.addParameter( 'alpha', [], @ispositive );
+  p.addParameter( 'W', [], @ispositive );
+  p.addParameter( 'nC', [], @ispositive );
+  p.addParameter( 'verbose', true, @(x) islogical(x) || isnumeric(x) );
   p.parse( varargin{:} );
   alpha = p.Results.alpha;
   W = p.Results.W;
   nC = p.Results.nC;
-  weights = p.Results.weights;
   verbose = p.Results.verbose;
 
-  if numel( alpha ) == 0, alpha = defaultAlpha; end
-  if numel( W ) == 0, W = defaultW; end
-  if numel( nC ) == 0, nC = defaultNc; end
-
-  nCoils = size( data, 2 );
-
-  if numel( weights ) == 0
-    weights = makePrecompWeights_2D( traj, sImg, ...
-      'alpha', alpha, 'W', W, 'nC', nC, 'verbose', verbose );
+  sData = size( kData );
+  nImgs = prod( sData(2:end) );
+  kData = reshape( kData, [ sData(1), nImgs ] );
+  if ismatrix( trajs )
+    weights = makePrecompWeights_2D( trajs, sImg, 'alpha', alpha, 'W', W, 'nC', nC );
+    trajs = repmat( trajs, [ 1 1 nImgs ] );
+    weights = repmat( weights, [ 1 nImgs ] );
+  else
+    trajs = reshape( trajs, [ sData(1) 2 nImgs ] );
+    weights = cell( 1, nImgs );
+    if verbose == true
+      disp( 'mri_gridRecon: creating gridding weights' );
+    end
+    parfor imgIndx = 1 : nImgs
+      weights{imgIndx} = makePrecompWeights_2D( trajs(:,:,imgIndx), sImg, ...
+        'alpha', alpha, 'W', W, 'nC', nC );
+    end
+    weights = cell2mat( weights );
   end
 
-  coilRecons = cell( 1, 1, nCoils );
-  parfor gCoilIndx = 1 : nCoils
-    coilData = data(:,gCoilIndx);
-    thisRecon = grid_2D( coilData(:), traj, sImg, weights, ...
+  recons = cell( 1, 1, nImgs );
+  p = parforProgress( nImgs );
+  parfor imgIndx = 1 : nImgs
+    if verbose == true, p.progress( imgIndx ); end   %#ok<PFBNS>
+    recons{imgIndx} = grid_2D( F, trajs(:,:,imgIndx), sImg, weights(:,:,imgIndx), ...
       'alpha', alpha, 'W', W, 'nC', nC );
-    coilRecons{1,1,gCoilIndx} = thisRecon;
   end
-  out = cell2mat( coilRecons );
+  p.clean;
+  recons = cell2mat( recons );
 
 end
