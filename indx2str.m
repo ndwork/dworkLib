cfbe8a2c4dba59fc17e0a757d4d6aad3438162d2
Nicholas Dwork <ndwork@hotmail.com>
2021-02-11 09:31:43 -0800

    Made N an optional parameter with indx2str.

--- indx2str.m
+++ indx2str.m
@@ -1,12 +1,14 @@
 
 function out = indx2str( indx, N )
-  % out = indx2str( indx, N )
+  % out = indx2str( indx [, N ] )
   %
   % Converts index to a string the appropriate size for N with padded zeros
-  % For example, if N is 100, will convert indx=88 into '0880';
+  % For example, if N is 100, will convert indx=88 into '088';
   %
   % Inputs:
   % indx - the number to convert into a string
+  %
+  % Optional Inputs:
   % N - the maximum indx that will be converted
   %
   % Outputs:
@@ -27,9 +29,17 @@ function out = indx2str( indx, N )
   % implied warranties of merchantability or fitness for a particular
   % purpose.
 
-  if nargin < 2, disp( 'out = indx2str( indx, N )' ); end
+  if nargin < 1, disp( 'Usage:  out = indx2str( indx [, N ] )' ); end
 
-  nDigits = floor( log10( N ) ) + 1;
+  if nargin < 2
+    if mod( indx, 10 ) == 0
+      nDigits = floor( log10( indx ) ) + 1;
+    else
+      nDigits = floor( log10( indx ) ) + 2;
+    end
+  else
+    nDigits = floor( log10( N ) ) + 1;
+  end
 
   formatSpec = [ '%', num2str(nDigits), '.', num2str(nDigits), 'i' ];
 
