e6c7da035e61fbfc3b47b6691d74a5ca6cb367bf
Nicholas Dwork <ndwork@hotmail.com>
2021-02-11 11:44:56 -0800

    Made quadRoots work with complex roots.

--- quadRoots.m
+++ quadRoots.m
@@ -1,7 +1,7 @@
 
 function roots = quadRoots( p, varargin )
   % roots = quadRoots( a, b, c ) or
-  % roots = quadRoots( p ) or
+  % roots = quadRoots( p )
   %
   % Numerically stable method of Determining the roots of a quadratic polynomial
   % Written according to equation 5.6.4 of Numerical Recipes in C
@@ -13,7 +13,7 @@ function roots = quadRoots( p, varargin )
   % b - a 1D array (or scalar)
   % c - a 1D array (or scalar)
   % p - a 3xN array (where N is the number of quadratics to solve)
-  %   Polynomials are p(1,:) + p(2) x + p(3) x^2
+  %   Polynomials are p(1,:) + p(2,:) x + p(3,:) x^2
   %
   % Written by Nicholas Dwork, Copyright 2018
   %
@@ -30,13 +30,23 @@ function roots = quadRoots( p, varargin )
   c = myParser.Results.c;
 
   if numel( c ) == 0
-    sb = sign(p(2,:));  sb( p(2,:)==0 ) = 1;
-    q = -0.5 * ( p(2,:) + sb .* sqrt( p(2,:).*p(2,:) - 4.*p(3,:).*p(1,:) ) );
+    d = p(2,:) .* p(2,:) - 4 .* p(1,:) .* p(3,:);  % discriminant
+    sqrtd = sqrt( d );
+    sb = sign( p(2,:) );
+    sb( d<0 ) = -sign( real( conj( p(2,d<0) ) .* sqrtd( d<0 ) ) );
+    sb( sb==0 ) = 1;
+
+    q = -0.5 * ( p(2,:) + sb .* sqrtd );
     roots = [ q ./ p(3,:)  p(1,:) / q ];
+
   else
     % a is p
-    sb = sign(b);  sb( sb==0 ) = 1;
-    q = -0.5 * ( b + sb .* sqrt( b.*b - 4 .* p .* c ) );
+    d = b .* b - 4 .* p .* c;  % discriminant
+    sqrtd = sqrt( d );
+    sb = sign( b );
+    sb( d<0 ) = -sign( real( conj( b( d<0 ) ) .* sqrtd( d<0 ) ) );
+    sb( sb==0 ) = 1;
+    q = -0.5 * ( b + sb .* sqrtd );
     roots = [ q./p; c./q; ];
   end
 
