3c51abe3f35950556241c246f1722f1624e2060c
Nicholas Dwork <ndwork@hotmail.com>
2021-02-18 14:29:24 -0800

    Minor change to comments of pogm.

--- pogm.m
+++ pogm.m
@@ -33,6 +33,8 @@ function [xStar,objectiveValues] = pogm( x, g, gGrad, proxth, varargin )
   %
   % Written by Nicholas Dwork - Copyright 2021
   %
+  % https://github.com/ndwork/dworkLib.git
+  %
   % This software is offered under the GNU General Public License 3.0.  It
   % is offered without any warranty expressed or implied, including the
   % implied warranties of merchantability or fitness for a particular
