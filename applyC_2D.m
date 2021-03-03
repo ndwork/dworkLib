930832f7850012732e84ee4a2f4f36e241f01421
Nicholas Dwork <ndwork@hotmail.com>
2021-03-03 11:21:42 -0800

    Upgraded new applyC_2D for makePrecompWeights_2D.

--- applyC_2D.m
+++ applyC_2D.m
@@ -1,6 +1,6 @@
 
 function out = applyC_2D( F, kTraj, N, kCy, kCx, Cy, Cx, varargin )
-  % out = applyC_2D( F, kTraj, N, kCy, kCx, Cy, Cx [, gridKs, 'type', type ] )
+  % out = applyC_2D( F, kTraj, N, kCy, kCx, Cy, Cx [, newKs ] )
   %
   % Applies a continuous circular convolution of a kernel as detailed in
   % http://nicholasdwork.com/tutorials/dworkGridding.pdf
@@ -15,7 +15,8 @@ function out = applyC_2D( F, kTraj, N, kCy, kCx, Cy, Cx, varargin )
   % Cx - 
   %
   % Optional Inputs:
-  %  gridKs - 
+  %  newKs - An nK x 2 array specifying the ky / kx (first / second column) coordinates
+  %          of the new points
   %  type -  by default, performs a circular convolution  If type=='noCirc',
   %          then it performs a regular(non-circular) convolution.
   %
@@ -29,37 +30,51 @@ function out = applyC_2D( F, kTraj, N, kCy, kCx, Cy, Cx, varargin )
   % purpose.
 
   p = inputParser;
-  p.addOptional( 'gridKs', [] );
+  p.addOptional( 'newKs', [] );
   p.parse( varargin{:} );
-  gridKs = p.Results.gridKs;
+  newKs = p.Results.newKs;
 
-  if numel( gridKs ) == 0
-    gridKs = size2fftCoordinates( N );
-    gridKy = gridKs{1};  gridKx = gridKs{2};
+  if numel( newKs ) == 0
+    newKs = size2fftCoordinates( N );
+    newKy = newKs{1};  newKx = newKs{2};
+    gridKsSupplied = false;
   else
-    gridKy = gridKs(:,1);  gridKx = gridKs(:,2);
+    newKy = newKs(:,1);  newKx = newKs(:,2);
+    gridKsSupplied = true;
   end
 
   kTrajY = kTraj(:,1);
   kyDists = min( cat( 3, ...
-    abs(   kTrajY         - gridKy' ), ...
-    abs( ( kTrajY + 1.0 ) - gridKy' ), ...
-    abs( ( kTrajY - 1.0 ) - gridKy' ) ), [], 3 );
+    abs(   kTrajY         - newKy' ), ...
+    abs( ( kTrajY + 1.0 ) - newKy' ), ...
+    abs( ( kTrajY - 1.0 ) - newKy' ) ), [], 3 );
   CValsY = interp1( kCy, Cy, kyDists, 'linear', 0 );
 
   kTrajX = kTraj(:,2);
   kxDists = min( cat( 3, ...
-    abs(   kTrajX         - gridKx' ), ...
-    abs( ( kTrajX + 1.0 ) - gridKx' ), ...
-    abs( ( kTrajX - 1.0 ) - gridKx' ) ), [], 3 );
+    abs(   kTrajX         - newKx' ), ...
+    abs( ( kTrajX + 1.0 ) - newKx' ), ...
+    abs( ( kTrajX - 1.0 ) - newKx' ) ), [], 3 );
   CValsX = interp1( kCx, Cx, kxDists, 'linear', 0 );
 
-  nTraj = size( kTraj, 1 );
-  out = zeros( [ N size(F,2) ] );
-  for trajIndx = 1 : nTraj
-    CValsYX = CValsY(trajIndx,:)' * CValsX(trajIndx,:);
-    thisF = reshape( F(trajIndx,:), [ 1 1 size(F,2) ] );
-    out = out + bsxfun( @times, thisF, CValsYX );
+  nFs = size( F,2 );
+  if gridKsSupplied == false
+    nTraj = size( kTraj, 1 );
+    out = zeros( [ numel(newKy) numel(newKx) size(F,2) ] );
+    for trajIndx = 1 : nTraj
+      CValsYX = CValsY(trajIndx,:)' * CValsX(trajIndx,:);
+      thisF = reshape( F(trajIndx,:), [ 1 1 nFs ] );
+      out = out + bsxfun( @times, thisF, CValsYX );
+    end
+
+  else
+    CValsYX = CValsY .* CValsX;
+    out = zeros( numel(newKx), nFs );
+    for indxF = 1 : nFs
+      FCValsYX = bsxfun( @times, F(:,indxF), CValsYX );
+      out(:,indxF) = transpose( sum( FCValsYX, 1 ) );
+    end
+
   end
 
 end
