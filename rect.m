1963651962a5fd01b18a78af67625efaf81bb637
Nicholas Dwork <ndwork@hotmail.com>
2021-03-19 19:05:26 -0700

    Adding rect.m to the repository.

---
+++ rect.m
@@ -0,0 +1,24 @@
+
+function out = rect( x, a )
+  % out = rect( x, a )
+  %
+  % outputs rect( x / a );
+  %
+  % Inputs:
+  % x - array of unscaled domain values
+  % a - either a scalar or array same size as x
+  %
+  % Outputs:
+  % out - array of rect values the same size as x
+  %
+  % Written by Nicholas Dwork - Copyright 2021
+  %
+  % https://github.com/ndwork/dworkLib.git
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  out = abs( x ./ a ) < 0.5;
+end
