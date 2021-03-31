352eb6324433e004366cb59b721271b522d5541b
Nicholas Dwork <ndwork@hotmail.com>
2021-03-31 15:27:47 -0700

    mri_gridRecon now assumes same weights for all trajectories when only one submitted.

--- mri_gridRecon.m
+++ mri_gridRecon.m
@@ -1,18 +1,20 @@
 
 function recons = mri_gridRecon( kData, trajs, sImg, varargin )
   % recons = mri_gridRecon( kData, trajs, sImg [, 'alpha', alpha, 'W', W, 'nC', nC, ...
-  %   'verbose', verbose ] )
+  %   'weights', weights, 'verbose', verbose ] )
   %
   % Performs a gridding recon for each coil
   %
   % Inputs:
-  % data - Several options:
-  %   A complex N x C x D1 x D2 x ... x DN array of data values
+  % kData - A complex N x C x D1 x D2 x ... x DN array of data values
   % trajs - a vector of N x 2 x C x D1 x D2 x ... x DN specifying the k-space trajectory
   %   If trajs is size N x 2, it is assumed the same trajectory was used for all
   %     other dimensions.
   %   C is the number of coils used during the acquisition.
   % sImg - the size of the output image
+  %
+  % Optional Inputs:
+  % weights - the density compensation weights to be used
   %
   % Output:
   % recons is an array of reconstructed images of size sImg(1) x sImg(2) x C x D1 x D2 x ... x DN
@@ -51,12 +53,10 @@ function recons = mri_gridRecon( kData, trajs, sImg, varargin )
   if ~isreal( trajs )
     trajs = [ real( trajs(:) ) imag( trajs(:) ) ];
   end
-
+  
   if numel( weights ) == 0
     if ismatrix( trajs )
       weights = makePrecompWeights_2D( trajs, sImg, 'alpha', alpha, 'W', W, 'nC', nC );
-      trajs = repmat( trajs, [ 1 1 nImgs ] );
-      weights = repmat( weights, [ 1 nImgs ] );
     else
       trajs = reshape( trajs, [ sData(1) 2 nImgs ] );
       weights = cell( 1, nImgs );
@@ -69,6 +69,13 @@ function recons = mri_gridRecon( kData, trajs, sImg, varargin )
       end
       weights = cell2mat( weights );
     end
+  end
+
+  if ismatrix( trajs )
+    trajs = repmat( trajs, [ 1 1 nImgs ] );
+  end
+  if ismatrix( weights )
+    weights = repmat( weights, [ 1 nImgs ] );
   end
 
   if nImgs > 1
