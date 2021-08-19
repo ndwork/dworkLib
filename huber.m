e451e48c3b2c49a54f5b35ca8a0f35fb87e00085
Nicholas Dwork <ndwork@hotmail.com>
2021-08-19 15:14:39 -0700

    Adding huber penalty to the repository.

---
+++ huber.m
@@ -0,0 +1,26 @@
+
+function out = huber( in, mu )
+  % out = huber( in, mu )
+  %
+  % Calculates the Huber penalty loss of in
+  %
+  % Inputs:
+  % in - a 1D array
+  % mu - the scalar Huber parameter
+  %
+  % Written by Nicholas Dwork - Copyright 2021
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular purpose.
+
+  if nargin < 2
+    disp( 'Usage:  out = huber( in, mu )' );
+    out = [];
+    return;
+  end
+
+  out = in .* in * 0.5 / mu;
+  out( abs( in ) > mu ) = abs( in( abs( in ) > mu ) ) - 0.5 * mu;
+
+end
