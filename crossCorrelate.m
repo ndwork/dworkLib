6a7a90697dfc9356b1eeda0326a51fd1abf97625
Nicholas Dwork <ndwork@hotmail.com>
2021-08-31 21:09:36 -0700

    Adding crossCorrelate to the repository.

---
+++ crossCorrelate.m
@@ -0,0 +1,51 @@
+
+function out = crossCorrelate( inArray, kernel )
+  % out = out = crossCorrelate( inArray, kernel )
+  %
+  % Calculates the cross correlation of inArray with kernel.
+  %   The origin of inArray is the first element.
+  %   The origin of kernel is the center element.
+  %
+  % Inputs:
+  % inArray - an N dimensional array
+  % kernel - a small N dimensional array
+  %
+  % Outputs:
+  % out - a N dimensional array of size max( size( inArray ), size( kernel ) )
+  %
+  % Written by Nicholas Dwork - Copyright 2021
+  %
+  % https://github.com/ndwork/dworkLib.git
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular purpose.
+
+  sA = size( inArray );
+  sK = size( kernel );
+  newSize = max( sA + sK -1 );
+
+  pad1 = zeros( newSize );
+  pad2 = zeros( newSize );
+  str2eval1 = 'pad1( 1:sA(1)';
+  str2eval2 = 'pad2( 1:sK(1)';
+  str2evalOut = 'out = out( 1:sA(1)';
+  for dim = 2 : numel( sA )
+    str2eval1 = [ str2eval1, ', 1:sA(', num2str(dim), ')' ];   %#ok<AGROW>
+    str2eval2 = [ str2eval2, ', 1:sK(', num2str(dim), ')' ];   %#ok<AGROW>
+    str2evalOut = [ str2evalOut, ', 1:sA(', num2str(dim), ')' ];  %#ok<AGROW>
+  end
+  str2eval1 = [ str2eval1, ' ) = inArray;' ];
+  str2eval2 = [ str2eval2, ' ) = kernel;' ];
+  str2evalOut = [ str2evalOut, ' );' ];
+
+  eval( str2eval1 );
+  eval( str2eval2 );
+
+  pad2 = circshift( pad2, -floor( sK / 2 ) );
+  pad2 = flipAboutIndx( pad2, ones( ndims( inArray ), 1 ) );
+
+  out = ifftn( fftn( pad1 ) .* fftn( pad2 ) );
+  eval( str2evalOut );
+end
+
