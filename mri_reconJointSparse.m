ed0e6088ca7d5c3e3208cd3375015d60a8e82d56
Nicholas Dwork <ndwork@hotmail.com>
2021-09-08 14:36:27 -0700

    Added comments to mri_reconJointSparse.

--- mri_reconJointSparse.m
+++ mri_reconJointSparse.m
@@ -17,6 +17,13 @@ function [recons,objectiveValues] = mri_reconJointSparse( data, traj, sImg, ...
   % nC - number of points to use in convolution kernel
   %
   % Written by Nicholas Dwork, Copyright 2020
+  %
+  % https://github.com/ndwork/dworkLib.git
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
 
   p = inputParser;
   p.addParameter( 'alpha', [], @ispositive );
