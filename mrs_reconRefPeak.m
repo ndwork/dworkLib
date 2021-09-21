f622940811be22d5560b81010c405a1bee35dc20
Nicholas Dwork <ndwork@hotmail.com>
2021-09-21 16:40:46 -0700

    Accomodating many more dimensions with mrs_reconRefPeak.

--- mrs_reconRefPeak.m
+++ mrs_reconRefPeak.m
@@ -1,13 +1,18 @@
 
-function [ recon, sMaps ] = mrs_reconRefPeak( kData )
-  % [ recon, sMaps ] = mrs_reconRefPeak( kData )
+function [ recon, sMaps ] = mrs_reconRefPeak( kData, varargin )
+  % [ recon, sMaps ] = mrs_reconRefPeak( kData [, 'kTraj', kTraj, 'sImg', sImg ] )
   %
   % Performs the peak reference reconstruction described in "Methodology for
   % improved detection of low concentration metabolites in MRS: optimised 
   % combination of signals from multi-element coil arrays" by Hall et al. (2014)
   %
   % Inputs:
-  % kData - array representing the Fourier values of size M x N x nSlices x nCoils x F
+  % kData - array representing the Fourier values of size M x N x nSlices x nCoils x N1 x N2 x ... x N
+  %         or nTraj x nSlices x nCoils x N1 x N2 x ... x N
+  %
+  % Optional Inputs:
+  % kTraj - An Nx2 array specifying (ky,kx) for each trajectory point
+  % sImg - A two element array specifying the size of the image to be reconstructed
   %
   % Written by Nicholas Dwork - Copyright 2021
   %
@@ -18,25 +23,44 @@ function [ recon, sMaps ] = mrs_reconRefPeak( kData )
   % implied warranties of merchantability or fitness for a particular
   % purpose.
 
-  coilSpectrums = fftshift( fftshift( uifft2( kData ), 1 ), 2 );
+  if nargin < 1
+    disp( 'Usage:  [ recon, sMaps ] = mrs_reconRefPeak( kData [, ''kTraj'', kTraj ] )' );
+    if nargout > 0, recon = []; end
+    if nargout > 1, sMaps = []; end
+    return
+  end
 
+  p = inputParser;
+  p.addParameter( 'kTraj', [], @isnumeric );
+  p.addParameter( 'sImg', [], @isnumeric );
+  p.parse( varargin{:} );
+  kTraj = p.Results.kTraj;
+  sImg = p.Results.sImg;
+
+  sKData = size( kData );
+  if numel( kTraj ) > 0
+    coilSpectrums = mri_reconGridding( kData, kTraj, sImg );
+    Ns = sKData(4:end);
+  else
+    coilSpectrums = fftshift( fftshift( uifft2( kData ), 1 ), 2 );
+    Ns = sKData(5:end);
+  end
+
+  sCSs = size( coilSpectrums );
+  coilSpectrums = reshape( coilSpectrums, [ sCSs(1:4) prod( sCSs(5:end) ) ] );
+
+  coilIndx = 4;
   freqIndx = ndims( coilSpectrums );
-  coilIndx = freqIndx - 1;
 
   maxImgs = max( coilSpectrums, [], freqIndx );
   sMaps = bsxfun( @times, maxImgs, 1 ./ sqrt( sum( maxImgs.^2, coilIndx ) ) );
 
-  if ndims( kData ) == 5
-    [ M, N, nSlices, nCoils ] = size( sMaps );
-    sMapped = bsxfun( @times, reshape( conj( sMaps ), [ M N nSlices nCoils ] ), coilSpectrums );
-  else
-    [ M, N, nCoils ] = size( sMaps );
-    sMapped = bsxfun( @times, reshape( conj( sMaps ), [ M N nCoils ] ), coilSpectrums );
-  end
+
+  [ M, N, nSlices, nCoils ] = size( sMaps );
+  sMapped = bsxfun( @times, reshape( conj( sMaps ), [ M N nSlices nCoils ] ), coilSpectrums );
 
   recon = sum( sMapped, coilIndx );
   sRecon = size( recon );
-  recon = reshape( recon, [ sRecon(1:coilIndx-1) sRecon(coilIndx+1:end) ] );
+  recon = reshape( recon, [ sRecon(1:coilIndx-1) Ns ] );
 end
-
 
