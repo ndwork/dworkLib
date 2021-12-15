641842cfcb8813bcae7b654556d6f613aaf19c27
Nicholas Dwork <ndwork@hotmail.com>
2021-12-15 10:53:16 -0700

    Needed to square mse in calcErgasMetric.

--- calcErgasMetric.m
+++ calcErgasMetric.m
@@ -32,10 +32,10 @@ function ergas = calcErgasMetric( color, monochrome, fused, varargin )
 
   sImg = size( fused );
   nPix = sImg(1)*sImg(2);
-  redMSE = norm( color(:,:,1) - fused(:,:,1), 2 ) / nPix;
-  greenMSE = norm( color(:,:,2) - fused(:,:,2), 2 ) / nPix;
-  blueMSE = norm( color(:,:,3) - fused(:,:,3), 2 ) / nPix;
-  monMSE = norm( monochrome - mean(fused,3), 2 ) / nPix;
+  redMSE = norm( color(:,:,1) - fused(:,:,1), 2 )^2 / nPix;
+  greenMSE = norm( color(:,:,2) - fused(:,:,2), 2 )^2 / nPix;
+  blueMSE = norm( color(:,:,3) - fused(:,:,3), 2 )^2 / nPix;
+  monMSE = norm( monochrome - mean(fused,3), 2 )^2 / nPix;
 
   tmpRed = color(:,:,1);    meanRed = mean( tmpRed(:) );
   tmpGreen = color(:,:,2);  meanGreen = mean( tmpGreen(:) );
@@ -43,7 +43,7 @@ function ergas = calcErgasMetric( color, monochrome, fused, varargin )
   meanMon = mean( monochrome(:) );
 
   ergas = 100*h/l * sqrt( 0.25 * ( ...
-    (redMSE/meanRed)^2 + (greenMSE/meanGreen)^2 + (blueMSE/meanBlue)^2 + ...
-    (monMSE/meanMon)^2 ) );
+    ( redMSE / meanRed )^2 + ( greenMSE / meanGreen )^2 + ...
+    ( blueMSE / meanBlue )^2 + ( monMSE / meanMon )^2 ) );
 
 end
