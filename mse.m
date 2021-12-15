c5c2cbabbad13f1667900eb724e6f3f212d68343
Nicholas Dwork <ndwork@hotmail.com>
2021-12-15 10:52:44 -0700

    Adding mse to the repository.

---
+++ mse.m
@@ -0,0 +1,23 @@
+
+function out = mse( est, true )
+  % Calculates the mean squared error of an estimate
+  %
+  % out = mse( est, true )
+  %
+  % Inputs:
+  % est - the array of estimate values
+  % true - an array of the same size as est that represents the correct values
+  %
+  % Outputs:
+  % out - the mean square error value
+  %
+  % Written by Nicholas Dwork - Copyright 2021
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  out = norm( est(:) - true(:) )^2 / numel( est );
+
+end
