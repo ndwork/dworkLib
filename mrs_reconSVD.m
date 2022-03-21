b19eef6255f6d85f783b05dadf67fd7abb7285e1
Nicholas Dwork <nicholasdwork@COHO-DworkMBP.local>
2022-03-21 12:21:11 -0600

    Adding mrs_reconSVD to the repository.

---
+++ mrs_reconSVD.m
@@ -0,0 +1,40 @@
+
+function out = mrs_reconSVD( coilSpectrums )
+  % out = mrs_reconSVD( coilSpectrums )
+  %
+  % Combines the spectrums from all coils into a single spectral image
+  % using the SVD method of "Spectral analysis of multichannel MRS data" by
+  % Sandgren et al.
+  %
+  % Inputs:
+  % coilSpectrums - an array of size sImg x nCoils x nF representing the Fourier value
+  %   of each spatial location and frequency bin
+  %
+  % Outputs:
+  % out - an array of size sImg x nF
+  %
+  % Written by Nicholas Dwork, Copyright 2022
+  %
+  % https://github.com/ndwork/dworkLib.git
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  sCoilSpectrums = size( coilSpectrums );
+  ndims = numel( sCoilSpectrums );
+  sImg = sCoilSpectrums( 1 : ndims - 2 );
+
+  nVoxels = prod(sImg);
+  coilSpectrums = reshape( coilSpectrums, [ nVoxels nCoils nF ] );
+
+  out = zeros( [ nVoxels nF ] );
+  for vox = 1 : nVoxels
+    theseSpectrums = squeeze( coilSpectrums( vox, :, : ) );
+    [u,s,~] = svd( theseSpectrums, 'econ', 'vector' );
+    out( vox, : ) = s( 1 ) * u( : , 1 );
+  end
+
+  out = reshape( out, [ sImg nF ] );
+end
