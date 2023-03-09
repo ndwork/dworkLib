b61408381a5fc30102b0382acbbf142fcd319c69
Nicholas Dwork <ndwork@gmail.com>
2023-03-09 13:13:08 -0700

    Adding ffth to the repository.

---
+++ ffth.m
@@ -0,0 +1,27 @@
+
+function out = ffth( in, varargin )
+  % out = ffth( in )
+  %
+  % Compute the adjoint of the FFT
+  %
+  % Written by Nicholas Dwork, Copyright 2023
+  %
+  % https://github.com/ndwork/dworkLib.git
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  p.addOptional( 'nFFT', [], @ispositive );
+  p.addOptional( 'dim', [], @ispositive );
+  p.parse( varargin{:} );
+  nFFT = p.Results.nFFT;
+  dim = p.Results.dim;
+
+  if numel( dim ) > 0
+    out = size( in, dim ) * ifft( in, nFFT, dim );
+  else
+    out = numel( in ) * ifft( in, nFFT, dim );
+  end
+end
