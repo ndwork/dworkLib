9e6698b2fb809f9687ad28386b02f58e753c6a62
Nicholas Dwork <ndwork@gmail.com>
2023-04-14 19:09:20 -0600

    Added tolerance to findValueBelowFraction.

--- findValueBelowFraction.m
+++ findValueBelowFraction.m
@@ -1,11 +1,14 @@
 
-function value = findValueBelowFraction( data, fraction )
-  % value = findValueBelowFraction( data, fraction )
+function value = findValueBelowFraction( data, fraction, varargin )
+  % value = findValueBelowFraction( data, fraction [, 'tol', tol ] )
   %
   % This function finds the value where fraction amount of the data is
   % above this value.
   % For example, if fraction is 0.35, then this function finds the value
   % where approximately 35% of the data is above this value.
+  %
+  % Inputs:
+  % tol - value is within tol of the answer
   %
   % Written by Nicholas Dwork - Copyright 2017
   %
@@ -13,6 +16,11 @@ function value = findValueBelowFraction( data, fraction )
   % is offered without any warranty expressed or implied, including the
   % implied warranties of merchantability or fitness for a particular
   % purpose.
+
+  p = inputParser;
+  p.addParameter( 'tol', 1d-8, @ispositive );
+  p.parse( varargin{:} );
+  tol = p.Results.tol;
 
   if nargin < 1
     disp( 'Usage: value = findValueBelowFraction( data, fraction )' );
@@ -24,5 +32,5 @@ function value = findValueBelowFraction( data, fraction )
   upperValue = max( data(:) );
 
   f = @( value ) fraction - sum( data(:) >= value ) / nData; 
-  value = binarySearch( f, lowerValue, upperValue, 'tol', 1d-8, 'nMax', Inf );
+  value = binarySearch( f, lowerValue, upperValue, 'tol', tol, 'nMax', Inf );
 end
