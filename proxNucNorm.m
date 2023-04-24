181003c7d1d9d59ad4fc52d7bc66d3e39629b6e1
Nicholas Dwork <ndwork@gmail.com>
2023-04-24 06:12:43 -0600

    Added ability to use previous u with proxNucNorm.

--- proxNucNorm.m
+++ proxNucNorm.m
@@ -1,12 +1,20 @@
 
-function out = proxNucNorm( in, thresh )
-  % out = proxNucNorm( in, thresh )
+function [ out, u ] = proxNucNorm( in, thresh, varargin )
+  % [ out, u ] = proxNucNorm( in, thresh )
   %
   % Returns the proximal operator of the nuclear norm of the input matrix
   %
   % Inputs:
   % in - an input matrix
   % thresh - the thresholding value
+  %
+  % Optional Inputs:
+  % u - the u matrix of the SVD decomposition to make code faster for tall and skinny
+  %     matrices
+  %
+  % Outputs:
+  % out - vector
+  % u - the u matrix of the svd
   %
   % Written by Nicholas Dwork - Copyright 2019
   %
@@ -20,7 +28,19 @@ function out = proxNucNorm( in, thresh )
     return;
   end
 
-  [u,s,v] = svd( in, 'econ', 'vector' );
+  p = inputParser;
+  p.addParameter( 'u', [], @isnumeric );
+  p.parse( varargin{:} );
+  u = p.Results.u;
+
+  if numel( u ) > 0
+    % Find s and v from svd of in*in and use previous u
+    % This should be faster for tall and skinny matrices
+    [~,s,v] = svd( in' * in, 'econ', 'vector' );
+    s = sqrt( s );
+  else
+    [u,s,v] = svd( in, 'econ', 'vector' );
+  end
 
   s = softThresh( s, thresh );  % Singular values are always real
 
