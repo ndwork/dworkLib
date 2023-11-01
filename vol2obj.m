17cdf864a4fce910b4f4ae65b3a638a982104d8e
Nicholas Dwork <ndwork@gmail.com>
2023-11-01 15:44:29 -0600

    Fixing vol2obj.

--- vol2obj.m
+++ vol2obj.m
@@ -1,10 +1,27 @@
 
 function vol2obj( vol, varargin )
+  % vol2obj( vol [, 'filename', filename, 'nTransparencies', nTransparencies, 'thresh',
+  %   thresh, 'verbose', true/false );
+  %
+  % Written by Nicholas Dwork - Copyright 2023
+  %
+  % https://github.com/ndwork/dworkLib.git
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
 
   p = inputParser;
   p.addParameter( 'filename', 'vol', @(x) true );
+  p.addParameter( 'nTransparencies', 1000, @ispositive );
+  p.addParameter( 'thresh', 0.05 );
+  p.addParameter( 'verbose', false, @(x) islogical(x) || isnumeric(x) );
   p.parse( varargin{:} );
   filename = p.Results.filename;
+  nTransparencies = p.Results.nTransparencies;
+  thresh = p.Results.thresh;
+  verbose = p.Results.verbose;
 
   if min( abs( vol(:) ) ) < 0 || max( abs( vol(:) ) ) > 1
     error( 'Assumes range of vol is [0 1]' );
@@ -14,24 +31,29 @@ function vol2obj( vol, varargin )
   mtlFile = [ filename, '.mtl' ];   mtlID = fopen( mtlFile, 'w' );
 
   % Create the MTL file with many transparencies
-  nTransparencies = 10;
   for t = 0 : nTransparencies
+    value = 1 - t / nTransparencies;
+    vString = num2str( value );
     fprintf( mtlID, [ 'newmtl trans_', num2str(t), '\n' ] );
-    fprintf( mtlID, '  Ka 1.000 1.000 1.000\n' );
-    fprintf( mtlID, '  Kd 1.000 1.000 1.000\n' );
+    fprintf( mtlID, [ '  Ka ', vString, ' ', vString, ' ', vString, '\n' ] );
+    fprintf( mtlID, [ '  Kd ', vString, ' ', vString, ' ', vString, '\n' ] );
     fprintf( mtlID, '  Ks 0.000 0.000 0.000\n' );
-    %fprintf( mtlID, [ '  d ', num2str(1-t/nTransparencies), '\n' ] );
-    fprintf( mtlID, [ '  Tr ', num2str(t/nTransparencies), '\n\n' ] );
+    %fprintf( mtlID, [ '  d ', num2str(t/nTransparencies), '\n' ] );
+    fprintf( mtlID, [ '  Tr ', vString, '\n\n' ] );
   end
-
-  sVol = size( vol );  
-  faces = zeros( [ sVol 6 4 ] );
+  
+  sVol = size( vol );
 
   fprintf( objID, '# vertices \n' );
   fprintf( objID, '\nmtllib vol.mtl\n');
 
   nVol = numel( vol );
   for i = 1 : nVol
+    if verbose == true  &&  mod( i+1, sVol(1)*sVol(2) ) == 0
+      disp([ 'Working on slice ', indx2str(z,sVol(3)), ' of ', num2str(sVol(3)) ]);
+    end
+
+    if vol( i ) < thresh, continue; end
     [y,x,z] = ind2sub( sVol, i );
 
     % We will make size faces for six sides of the cube
@@ -47,25 +69,13 @@ function vol2obj( vol, varargin )
     fprintf( objID, [ 'v ', num2str(x),   ' ', num2str(y+1), ' ', num2str(z+1), '\n' ] );
     fprintf( objID, '\n' );
 
-    % Now we will store an array of faces for later
-    v = (i-1) * 8 + 1;
-    faces(y,x,z,1,:) = [ v   v+1 v+2 v+3 ];
-    faces(y,x,z,2,:) = [ v+1 v+5 v+6 v+2 ];
-    faces(y,x,z,3,:) = [ v+2 v+6 v+7 v+3 ];
-    faces(y,x,z,4,:) = [ v   v+1 v+5 v+4 ];
-    faces(y,x,z,5,:) = [ v   v+4 v+7 v+3 ];
-    faces(y,x,z,6,:) = [ v+4 v+5 v+6 v+7 ];
-  end
-
-  fprintf( objID, '\n' );
-  fprintf( objID, '# faces \n' );
-  for i = 1 : nVol
-    [y,x,z] = ind2sub( sVol, i );
     fprintf( objID, [ 'usemtl trans_', num2str( round( vol(i) * nTransparencies ) ), '\n' ] );
-    for face = 1 : 6
-      faceString = arrayfun( @num2str, faces(y,x,z,face,:), 'UniformOutput', 0 );
-      fprintf( objID, [ 'f ', strjoin( faceString, ', ' ), '\n' ] );
-    end
+    fprintf( objID, 'f -8 -7 -6 -5\n' );
+    fprintf( objID, 'f -7 -3 -2 -6\n' );
+    fprintf( objID, 'f -6 -2 -1 -5\n' );
+    fprintf( objID, 'f -8 -7 -3 -4\n' );
+    fprintf( objID, 'f -8 -4 -1 -5\n' );
+    fprintf( objID, 'f -4 -3 -2 -1\n' );
     fprintf( objID, '\n' );
   end
 
