582418cd968eac883430572d2541fa8e43cb424f
Nicholas Dwork <ndwork@gmail.com>
2024-02-29 10:17:31 -0700

    Adding findMinMAE and findMinMSE to the repository.

---
+++ findMinMAE.m
@@ -0,0 +1,31 @@
+
+function out = findMinMAE( recon, trueRecon, varargin )
+  % Result is min_k MAE( k * recon - trueRecon )
+  %
+  % out = findMinMAE( recon, trueRecon [, 'verbose', true/false ] )
+  %
+  % Written by Nicholas Dwork, Copyright 2024
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  if nargin < 2
+    disp( 'Usage:  out = findMinMAE( recon, trueRecon [, ''verbose'', true/false ] )' );
+    if nargout > 0, out = []; end
+    return;
+  end
+
+  p = inputParser;
+  p.addParameter( 'verbose', true );
+  p.parse( varargin{:} );
+  verbose = p.Results.verbose;
+
+  f = @(k) mse( k*recon, trueRecon );
+
+  LB = 0;
+  UB = max( recon(:) ) / mean( trueRecon(:) ) * 10;
+
+  out = goldenSectionSearch( f, LB, UB, 'tol', 1d-6, 'verbose', verbose );
+end
