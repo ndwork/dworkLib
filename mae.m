68faf2be19df3d18f207ab9b40bc4baf9d1494ba
Nicholas Dwork <ndwork@gmail.com>
2024-02-29 10:18:42 -0700

    Adding mae to the repository.

--- mae.m
+++ mae.m
@@ -1,17 +1,17 @@
 
-function out = mse( est, true )
-  % Calculates the mean squared error of an estimate
+function out = mae( est, true )
+  % Calculates the mean absolute error of an estimate
   %
-  % out = mse( est, true )
+  % out = mae( est, true )
   %
   % Inputs:
   % est - the array of estimate values
   % true - an array of the same size as est that represents the correct values
   %
   % Outputs:
-  % out - the mean square error value
+  % out - the mean absolute error value
   %
-  % Written by Nicholas Dwork - Copyright 2021
+  % Written by Nicholas Dwork - Copyright 2024
   %
   % This software is offered under the GNU General Public License 3.0.  It
   % is offered without any warranty expressed or implied, including the
@@ -19,10 +19,11 @@ function out = mse( est, true )
   % purpose.
 
   if nargin < 2
-    disp( 'Usage:  out = mse( est, true )' );
+    disp( 'Usage:  out = mae( est, true )' );
     if nargout > 0, out = []; end
     return;
   end
 
-  out = norm( est(:) - true(:) )^2 / numel( est );
+  out = norm( est(:) - true(:), 1 )^2 / numel( est );
+
 end
