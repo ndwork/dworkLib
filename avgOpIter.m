dfe03f943637b07e374f98d4e82c3fdac69769f9
Nicholas Dwork <ndwork@gmail.com>
2024-03-10 13:38:26 -0600

    Fixing verbosity statement in avgOpIter.

--- avgOpIter.m
+++ avgOpIter.m
@@ -3,11 +3,21 @@ function [x,objValues] = avgOpIter( x0, S, varargin )
   % Implements and averaged opterator iteration.  See "Line Search for Averaged
   % Operator Iteration" by Gisellson et al. (2016)
   %
-  % x = avgOpIter( x0, S [, 'alpha', alpha, 'maxIter', maxIter ] )
+  % x = avgOpIter( x0, S [, 'alpha', alpha, 'N', N ] )
   %
   % Inputs:
   % x0 - the initial guess
   % S - either a matrix or a a function handle that is the non-expansive operator
+  %
+  % Optional Inputs:
+  % alpha - the scalar for the combination of the averaged operator iteration (default is 0.5)
+  % N - the number of iterations to run (default is 100)
+  % objFunction - a function handle to the objective function
+  % printEvery - print verbose statements every printEvery iterations
+  %
+  % Outputs:
+  % x - the value of the domain variable after all iterations are complete
+  % objValues - the value of objFunction evaluated on x at each iteration
   %
   % Written by Nicholas - Copyright 2024
   %
@@ -22,11 +32,13 @@ function [x,objValues] = avgOpIter( x0, S, varargin )
   p.addParameter( 'alpha', 0.5, @(x) x>0 && x<1 );
   p.addParameter( 'N', 100, @ispositive );
   p.addParameter( 'objFunction', [] );
+  p.addParameter( 'printEvery', 1, @ispositive );
   p.addParameter( 'verbose', false );
   p.parse( varargin{:} );
   alpha = p.Results.alpha;
   N = p.Results.N;
   objFunction = p.Results.objFunction;
+  printEvery = p.Results.printEvery;
   verbose = p.Results.verbose;
 
   if nargout > 1
@@ -52,7 +64,7 @@ function [x,objValues] = avgOpIter( x0, S, varargin )
       objValues( optIter ) = objValue;
     end
 
-    if verbose == true
+    if verbose == true && mod( optIter, printEvery ) == 0
       outStr = [ 'avgOpIter: Completed ', indx2str(optIter,N), ' of ', num2str(N) ];
       if numel( objFunction ) > 0
         outStr = [ outStr, '  objective: ', num2str( objValue ) ];   %#ok<AGROW>
