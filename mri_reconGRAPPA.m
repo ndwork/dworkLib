9a947bca187ff5bb746e56032ecba577c775c1d8
Nicholas Dwork <ndwork@gmail.com>
2024-03-14 15:51:09 -0600

    Adding mri_reconSPIRiT to the repository.

--- mri_reconGRAPPA.m
+++ mri_reconGRAPPA.m
@@ -74,7 +74,7 @@ function k_out = mri_reconGRAPPA( k_in, kernel_sz, acr_sz )
   end
 
   % get auto-calibration region
-  acr = get_acr( k_in, acr_sz );
+  acr = cropData( k_in, [ acr_sz size(k_in,3) ] );
 
   % for a given size of kernel, we'll have edge cases depending on the
   % sampling pattern
@@ -142,72 +142,6 @@ function out = fill_points(array, kernel_array, kernel, weights)
   
     fillpt = weights.' * squeeze(pts);
     out(ri, ci, :) = fillpt;
-  end
-
-end
-
-
-function acr = get_acr(k_in, acr_sz)
-  % get_acr get ACR points from data
-  % 
-  % The point of this helper function is to retrieve the fully sampled region
-  % in the middle of k-space - the auto-calibration region (ACR).
-  % This takes the undersampled k-space data and the size of the ACR and
-  % returns the values in the ACR to be used for solving for the weights in
-  % GRAPPA or other algorithms
-  %
-  % Author: Alex McManus
-  % *********************
-  %   Input Parameters:
-  % *********************
-  %
-  %    k_in:  A 3D (size ny x nx x ncoils) array of measured k-space. The
-  %    assumption in GRAPPA is that there is a fully sampled region in the
-  %    center of the image. The center point convention is as follows:
-  %
-  %    if our array is odd size, we choose the center point
-  %           o o o x o o o
-  %    if our array is even size, we choose length / 2 + 1
-  %           o o o x o o 
-  %
-  %    acr_sz: A 2 element vector containing the dimensions of the ACR. ACR
-  %    must be odd sized in both directions.
-  %
-  % *********************
-  %   Output Variables:
-  % *********************
-  %
-  %    acr: A 3D (size ky x kx x ncoils) array of k-space values for the
-  %    auto-calibration region.
-  
-  ny = size(k_in, 1);
-  nx = size(k_in, 2);
-  
-  acr_dy = acr_sz(1);
-  acr_dx = acr_sz(2);
-  
-  if mod(acr_dx, 2) ~= 1 || mod(acr_dy, 2) ~= 1
-    error('ACR Size should be odd in both directions');
-  end
-  
-  acr_dx = (acr_dx - 1)/2;
-  acr_dy = (acr_dy - 1)/2;
-  
-  % we need to make sure that we have the correct center
-  % if our array is odd size, we choose the center point
-  % o o o x o o o
-  % if our array is even size, we choose length / 2 + 1
-  % o o o x o o 
-  center_y = ceil((ny+1)/2); 
-  center_x = ceil((nx+1)/2);
-  
-  ypts = center_y - acr_dy : center_y + acr_dy;
-  xpts = center_x - acr_dx : center_x + acr_dx;
-  
-  acr = k_in(ypts, xpts, :); 
-  
-  if sum(acr == 0, 'all') > 0
-    error('Auto-Calibration Region not fully sampled.');
   end
 
 end
---
+++ mri_reconSPIRiT.m
@@ -0,0 +1,275 @@
+function out = mri_reconSPIRiT( kData, kernel_sz, acr_sz, varargin )
+  %
+  % *********************
+  %   Input Parameters:
+  % *********************
+  %
+  %    kData:  A 3D (size ny x nx x ncoils) array of measured k-space. The
+  %    assumption in GRAPPA is that there is a fully sampled region in the
+  %    center of the image. The center point convention is as follows:
+  %
+  %    if our array is odd size, we choose the center point
+  %           o o o x o o o
+  %    if our array is even size, we choose length / 2 + 1
+  %           o o o x o o 
+  %
+  %    kernel_sz: a 2 element vector containing the dimensions of the kernel.
+  %    We use MATLAB dimensions, so it's [row column]
+  %    For example, a 3x1 kernel will fit inside a 5x5 ACR as:
+  %                 x o o o o
+  %                 x o o o o
+  %                 x o o o o
+  %                 o o o o o 
+  %                 o o o o o 
+  %    acr_sz: A 2 element vector containing the dimensions of the ACR. ACR
+  %    must be odd sized in both directions.
+  %
+  % Written by Alex McManus - Copyright 2022
+  %
+  % https://github.com/ndwork/dworkLib.git
+  %
+  % This software is offered under the GNU General Public License 3.0.  It is offered without any
+  % warranty expressed or implied, including the implied warranties of merchantability or fitness
+  % for a particular purpose.
+
+  acr = cropData( kData, acr_sz );
+  weights = mri_reconSPIRiT_get_weights( acr, kernel_sz );
+  
+  x0 = mri_reconGRAPPA( kData, kernel_sz, acr_sz );
+
+  idx_acq = kData~=0;
+
+  y_data = kData(idx_acq);
+  eps = 1e-2;
+
+  checkProx = 1;
+  if checkProx
+    proxTest = @(in, sc) projB0(in, sqrt(eps));
+    test1 = proxh(x0, 0);
+    test2 = proxAffine(proxTest, x0, @applyD, -1*y_data, 1);
+    
+    err = norm(test1(:) - test2(:))/norm(test2(:));
+    if abs(err) > 1e-6
+      error('Something wrong with proximal operator');
+    end
+  end
+
+  % [xStar, objVal, relDiffs] = fista(0*x0(:), @grad_g, @proxh, 'g', @normG, 'h', @h, 'N', 100, ...
+  %   'verbose', true, 't', 0.05 );
+  [xStar, objVal, relDiffs] = fista_wLS(x0(:), @normG, @grad_g, @proxh, 'h', @h, 'N', 100, ...
+    'verbose', true, 't0', 0.01);
+
+  out = reshape( xStar, size(kData) );
+
+  function out = proxh(in, sc)
+    din = in(idx_acq) - y_data;
+    tmp_proj = projB0(din, sqrt(eps));
+    tmp2 = din - tmp_proj;
+    out = zeros(size(kData));
+    out(idx_acq) = tmp2;
+    out = in(:) - out(:);
+  end
+
+  function out = applyG(in, op)
+    in = reshape(in, size(kData));
+  if strcmp(op, 'transp')
+    out = spirit_conv_adj(in, weights);
+  else
+    out = spirit_conv(in, weights);
+  end
+  out = out(:);
+  end
+
+  function out = h(x)
+    out = ind(x, y_data, sqrt(eps));
+  end
+
+  function out = ind(x, y, r)
+    dx = x(idx_acq);
+    if norm(dx - y) < r
+      out = 0;
+    else
+      out = Inf;
+    end
+  end
+
+  function out = projB(x, y, r)
+    dxy = x - y;
+    if norm(dxy) < r
+      out = x;
+    else
+      out = dxy * (r / norm(dxy)) + y;
+    end
+  end
+
+function out = projB0(x, r)
+    if norm(x) < r
+      out = x;
+    else
+      out = x * (r / norm(x));
+    end
+  end
+
+  function out = normG(in)
+    tmpg = applyG(in, 'notransp');
+    out = 0.5 * norm(tmpg(:) - in(:))^2;
+  end
+
+  function out = grad_g(in)
+    tmpgrad = applyG(in, 'notransp') - in;
+    out = applyG(tmpgrad, 'transp') - tmpgrad;
+    out = out(:);
+  end
+
+  function out = applyD(in, op)
+    if strcmp(op, 'transp')
+      out = 0*kData;
+      out(idx_acq) = in;
+    else
+      out = in(idx_acq);
+    end
+  end
+
+end
+
+
+%-------------------------
+%--- Support Functions ---
+%-------------------------
+
+function out = mri_reconSPIRiT_get_points( pt_idx, array, kernel_sz )
+  % mri_reconSPIRiT_get_points helper function to retrieve correct points
+  % This function is used both to set up the weights and get the appropriate points and
+  % to interpolate with when filling in k-space.
+  % It's slightly different than the GRAPPA version because of the points we need to grab
+  %
+  % What about if we know that the kernel is square and just need the size
+  %
+  % Author: Alex McManus
+  % *********************
+  %   Input Parameters:
+  % *********************
+  %
+  %     pt_idx: A 3 element vector containing the [row column coilidx] of a specific
+  %     point. Coil index matters here since for solving for the weights, we
+  %     use the point at [row column] from the other coils.
+  %         NOTE: this is different than grappa
+  %
+  %     array: the full 3D (ny x nx x ncoils) kspace data
+  %
+  %     kernel_sz: a 2 element array containing the [row column] size of the
+  %     kernel. for spirit we'll always use this kernel completely filled in
+  %     except for the center point
+  %
+  % *********************
+  %   Output Variables:
+  % ********************* 
+  %
+  %    out: the set of points for the input pt_idx over all the coils
+
+  py = pt_idx(1);
+  px = pt_idx(2);
+  
+  k_y = kernel_sz(1);
+  k_x = kernel_sz(2);
+
+  kdy = (k_y - 1) / 2;
+  kdx = (k_x - 1) / 2;
+
+  ypts = py-kdy:py+kdy;
+  xpts = px-kdx:px+kdx;
+
+  pts = array(ypts, xpts, :);
+  out = pts(:);
+end
+
+
+function [W, A, B] = mri_reconSPIRiT_get_weights(acr, kernel_sz)
+  %new_spirit_get_weights lets try this again
+  
+  % auto-calibration region dimensions
+  szAcrY = size(acr, 1);
+  szAcrX = size(acr, 2);
+  ncoils = size(acr, 3);
+  
+  % kernel dimensions
+  szKerY = kernel_sz(1);
+  szKerX = kernel_sz(2);
+  
+  n = szKerY*szKerX;
+  
+  % centering
+  center_y = (szKerY - 1)/2;
+  center_x = (szKerX - 1)/2;
+  
+  % number of sliding kernel fits in the ACR
+  numfits_x = szAcrX - szKerX + 1;
+  numfits_y = szAcrY - szKerY + 1;
+  
+  npoints = szKerY*szKerX*ncoils;
+  W = zeros([kernel_sz ncoils ncoils]);
+  A = zeros([numfits_x*numfits_y npoints]);
+  
+  for coilIdx = 1:ncoils
+    B = zeros( [numfits_x*numfits_y 1] );
+    b_idx = 1;
+    for x = 1:numfits_x
+      for y = 1:numfits_y
+        B(b_idx) = acr( y + center_y, x + center_x, coilIdx);
+        pts = mri_reconSPIRiT_get_points([y+center_y x+center_x coilIdx], acr, kernel_sz);
+        A(b_idx, :) = pts;
+        b_idx = b_idx + 1;
+      end
+    end
+  
+    dk = zeros([szKerY szKerX ncoils]); 
+    dk((end+1)/2, (end+1)/2, coilIdx) = 1;
+    smp = ones(size(dk));
+    smp( dk == 1 ) = 0;
+    idxA = find(smp);
+  
+    A2 = A(:, idxA);
+    rk = A2 \ B;
+    
+    ker = zeros(size(dk));
+    ker(idxA) = rk;
+
+    W(:, :, :, coilIdx) = ker;
+  end
+end
+
+
+function out = spirit_conv(array, weights)
+  nCoils = size(array, 3);
+  out = 0 * array;
+  
+  for coil = 1:nCoils
+    wi = weights(:, :, :, coil);
+    for getCoil = 1:nCoils
+      wij = wi(:, :, getCoil);
+      res = filter2( wij, array(:, :, getCoil), 'same' );
+      out(:, :, coil) = out(:, :, coil) + res;
+    end
+  end
+
+end
+
+
+function out = spirit_conv_adj(array, weights)
+  nCoils = size(array, 3);
+  out = 0*array;
+  
+  for coil = 1:nCoils
+    wi = squeeze(weights(:, :, coil, :));
+    for getCoil = 1:nCoils
+      wij = wi(:, :, getCoil);
+      wij_conj = conj(wij);
+      %ker = flipud( fliplr(wij_conj) );
+      ker = rot90( wij_conj, 2 );
+      res = filter2( ker, array(:, :, getCoil), 'same' );
+      out(:, :, coil) = out(:, :, coil) + res;
+    end
+  end
+
+end
+
