37bc9ca5562b513fe96103510e0554802f21d5cb
Nicholas Dwork <ndwork@gmail.com>
2024-03-14 16:13:11 -0600

    Adding projectOntoBall to the repository.

---
+++ projectOntoBall.m
@@ -0,0 +1,33 @@
+
+function out = projectOntoBall( x, r, p )
+  % Project onto a ball of radius r
+  %
+  % out = projectOntoBall( x, r [, p ] )
+  %
+  % Inputs -
+  % x - 1D array that is the vector to project
+  % r - the radius of the ball to project onto
+  %
+  % Optional inputs:
+  % p - projection with respect to the Lp norm.
+  %
+  % Outputs:
+  % out - a 1D array that is the projected vector
+  %
+  % Written by Nicholas Dwork, Copyright 2024
+  %
+  % This software is offered under the GNU General Public License 3.0.  It
+  % is offered without any warranty expressed or implied, including the
+  % implied warranties of merchantability or fitness for a particular
+  % purpose.
+
+  if nargin < 3, p = 2; end
+
+  xNorm = norm( x, p );
+
+  if xNorm < r
+    out = x;
+  else
+    out = x * ( r / xNorm );
+  end
+end
