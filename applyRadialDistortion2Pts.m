4f9e8ff5cebbe907d5527a6cd6fcd143ab3829ab
Nicholas Dwork <ndwork@gmail.com>
2025-10-22 11:02:20 -0600

    The search was necessary and now results are much more numerically precise.

--- applyRadialDistortion2Pts.m
+++ applyRadialDistortion2Pts.m
@@ -1,6 +1,6 @@
 
-function outPts = applyRadialDistortion2Pts( pts, ks, c, varargin )
-  % out = applyRadialDistortion2Pts( pts, k, c [, 'dir', dir ] )
+function outPts = applyRadialDistortion2Pts( pts, ks, varargin )
+  % out = applyRadialDistortion2Pts( pts, k[, c, 'dir', dir ] )
   %
   % Written according to section 7.4 of Multiple View Geometry, 2nd edition
   % by Hartley and Zisserman
@@ -14,6 +14,7 @@ function outPts = applyRadialDistortion2Pts( pts, ks, c, varargin )
   %
   % Optional Inputs:
   % dir - if 1, applies radial distortion.  If -1, undoes radial distortion
+  % tol - tolerance to end fixed point iteration
   %
   % Outputs:
   % out - the image with radial distortion applied
@@ -28,20 +29,43 @@ function outPts = applyRadialDistortion2Pts( pts, ks, c, varargin )
   % purpose.
 
   p = inputParser;
+  p.addOptional( 'c', zeros(2,1), @(x) isempty(x) || isnumeric(x) );
   p.addParameter( 'dir', 1, @(x) x == 1 || x == -1 );
+  p.addParameter( 'maxIter', 50, @ispositive );
+  p.addParameter( 'tol', 1d-10, @ispositive );
   p.parse( varargin{:} );
+  c = p.Results.c;
   dir = p.Results.dir;
+  maxIter = p.Results.maxIter;
+  tol = p.Results.tol;
 
   rs = LpNorms( bsxfun( @minus, pts, c' ), 2, 2 );
   Ls = findLs( rs, ks );
 
-  if dir == 1
+  if dir == -1
     % outPts are the "corrected" undistorted points
-    outPts = bsxfun( @plus, bsxfun( @times, bsxfun( @minus, pts, c' ), Ls ), c' );
+    % Note: we don't want the Ls for the distorted values, which is what we would get by calling findLs here.
+    % Instead, we want the Ls that exist for the undistorted values.  So we need to do an optimization.
+    % Every radius relationship satisfies rUndistort = rDistort * L( rUndistort ).
+    % Equivalently, rDistort = rUndistort / L( rUndistort ).
+    % We solve for rUndistort using fixed point iteration
+
+    rUndistort = rs; % initial guess
+    for iter = 1 : maxIter
+      betterLs = findLs( rUndistort, ks );
+      betterLs( rs == 0 ) = 1;
+      rUndistortNew = rs .* betterLs;
+
+      dr = abs( rUndistortNew - rUndistort );
+      rUndistort = rUndistortNew;
+      if all( dr < tol ), break; end
+    end
+
+    outPts = bsxfun( @plus, bsxfun( @times, bsxfun( @minus, pts, c(:)' ), betterLs ), c(:)' );
 
   else
     % outPts are distorted with radial distortion
-    outPts = bsxfun( @plus, bsxfun( @rdivide, bsxfun( @minus, pts, c' ), Ls ), c' );
+    outPts = bsxfun( @plus, bsxfun( @rdivide, bsxfun( @minus, pts, c(:)' ), Ls ), c(:)' );
   end
 end
 
@@ -54,3 +78,4 @@ function Ls = findLs( rs, ks )
     if kIndx < numel( ks ), rPower = rPower .* rs; end
   end
 end
+
